dee25ab84d1406da2c92ddbc06dd4279b918c271
artonson <cocos@confa@17>
2019-07-03 22:22:03 +0300

    add build_singularity.sh

---
+++ docker/build_singularity.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# example launch string:
+# ./build_singularity.sh [-p]
+
+module load apps/singularity-3.2.0
+
+SIMAGES_DIR=/gpfs/gpfs0/3ddl/singularity-images
+
+[[ -d ${SIMAGES_DIR} ]] || mkdir ${SIMAGES_DIR}
+
+IMAGE_NAME="artonson/sharp_features"
+IMAGE_VERSION="latest"
+IMAGE_NAME_TAG="${IMAGE_NAME}:${IMAGE_VERSION}"
+SIMAGE_FILENAME="${SIMAGES_DIR}/$(echo ${IMAGE_NAME_TAG} | tr /: _).sif"
+
+echo "******* PULLING IMAGE FROM DOCKER HUB AND BUILDING SINGULARITY IMAGE *******"
+singularity pull "${SIMAGE_FILENAME}" "docker://${IMAGE_NAME_TAG}"
