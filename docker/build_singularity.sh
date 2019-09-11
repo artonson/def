f59681b1d1dd1a0a973a339218a127eb6c02205c
Alexey Artemov <a.artemov@skoltech.ru>
2019-09-11 13:13:22 +0300

    add -f switch to the build_singularity script for overwriting the existing images

--- docker/build_singularity.sh
+++ docker/build_singularity.sh
@@ -4,7 +4,8 @@ set -e
 set -x
 
 # example launch string:
-# ./build_singularity.sh [-p]
+# ./build_singularity.sh [-f]
+#     -f:       overwrite the existing singularity image (false by default)
 
 module load apps/singularity-3.2.0
 
@@ -18,4 +19,9 @@ IMAGE_NAME_TAG="${IMAGE_NAME}:${IMAGE_VERSION}"
 SIMAGE_FILENAME="${SIMAGES_DIR}/$(echo ${IMAGE_NAME_TAG} | tr /: _).sif"
 
 echo "******* PULLING IMAGE FROM DOCKER HUB AND BUILDING SINGULARITY IMAGE *******"
-singularity pull "${SIMAGE_FILENAME}" "docker://${IMAGE_NAME_TAG}"
+if echo $* | grep -e "-f" -q
+then
+    singularity pull -F "${SIMAGE_FILENAME}" "docker://${IMAGE_NAME_TAG}"
+else
+    singularity pull "${SIMAGE_FILENAME}" "docker://${IMAGE_NAME_TAG}"
+fi
