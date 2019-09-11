c2315969b6e08bd13068c76ce498aa819274ea71
Gleb Bobrovskikh <bobrovskihg@gmail.com>
2019-09-11 23:46:44 +0300

    added __getitem__ to ABCChunk + corrected some mistakes
    TODO1: exclude code dublicates and reimplement it with more smart functional
    TODO2: replace names_list in ABCFile with list and tree

--- sharpf/data/abc_data.py
+++ sharpf/data/abc_data.py
@@ -1,4 +1,5 @@
-from collections import Iterable, ByteString, defaultdict
+from collections.abc import Iterable, ByteString, Mapping
+from collections import defaultdict
 from contextlib import ExitStack, AbstractContextManager
 from itertools import groupby, islice
 from abc import ABC, abstractmethod
@@ -10,7 +11,7 @@ import os
 
 import py7zlib
 
-from utils.namedtuple import namedtuple_with_defaults as namedtuple
+from sharpf.utils.namedtuple import namedtuple_with_defaults as namedtuple
 
 
 class ABCModality(Enum):
@@ -25,7 +26,7 @@ class ABCModality(Enum):
 
 ALL_ABC_MODALITIES = [modality.value for modality in ABCModality]
 ABC_7Z_FILEMASK = 'abc_{chunk}_{modality}_v{version}.7z'  # abc_0000_feat_v00.7z
-ABC_7Z_REGEX = 'abc_(\d{4})_([a-z-]+)_v(\d{2}).7z' # abc_0000_feat_v00.7z
+ABC_7Z_REGEX = 'abc_(\d{4})_([a-z-]+)_v(\d{2}).7z'  # abc_0000_feat_v00.7z
 ABC_INAR_FILEMASK = '{dirname}/{dirname}_{hash}_{modalityex}_{number}.{ext}'  # '00000002/00000002_1ffb81a71e5b402e966b9341_features_001.yml'
 
 def _compose_filemask(chunks, modalities, version):
@@ -60,39 +61,30 @@ def _extract_inar_id(pathname):
     dirname, hash, modalityex, number = name.split('_')
     return '_'.join([dirname, hash, number])
 
-class AbstractABC(ABC):
-    def __init__(self, filename):
-        self.filename = filename
-        self.modality = _extract_modality(filename)
-        assert self.modality in ALL_ABC_MODALITIES, 'unknown modality: "{}"'.format(self.modality)
-        self._reset_handles()
+
+class AbstractABCDataHolder(Iterable, AbstractContextManager, ABC):
+    """ An abstract parent class inherited from childred which operate with ABC 7z files  """
     
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self._isopen():
-            self._close()
+            self.close()
     
     def __enter__(self):
         self._open()
         return self
 
-    def _reset_handler(self):
-        pass
-
     @abstractmethod
-    def _close(self):
-        pass
+    def _unset_handles(self): pass
     @abstractmethod
-    def _isopen(self):
-        pass
+    def close(self): pass
     @abstractmethod
-    def _open(self):
-        pass
+    def _isopen(self): pass
     @abstractmethod
-    def __iter__(self):
-        pass
+    def _open(self): pass
+    @abstractmethod
+    def __iter__(self): pass
     #@abstractmethod
-    #def __getitem__(self):
-    #    pass
+    #def __getitem__(self): pass
 
 # ABCItem is the primary data instance in ABC, containing
 # all the modalities in the form of bitstreams, supporting
@@ -103,22 +95,28 @@ ABCItem = namedtuple(
     defaults=(None for modality in ALL_ABC_MODALITIES))
 
 
-class ABC7ZFile(AbstractABC):
+class ABC7ZFile(AbstractABCDataHolder):
     """A helper class for reading 7z files."""
 
-    def _reset_handles(self):
+    def __init__(self, filename):
+        self.filename = filename
+        self.modality = _extract_modality(filename)
+        assert self.modality in ALL_ABC_MODALITIES, 'unknown modality: "{}"'.format(self.modality)
+        self._unset_handles()
+
+    def _unset_handles(self):
         self.file_handle = None
         self.archive_handle = None
-        self._names_list = None
+        self._names = None
 
     def _open(self):
-        self.file_handle = open(self.filename, 'rb')
+        self.file_handle = open(self.filename, 'rb') 
         self.archive_handle = py7zlib.Archive7z(self.file_handle)
-        self._names_list = self.archive_handle.getnames()
+        self._names = self.archive_handle.getnames() # rewrite with list and tree
 
-    def _close(self):
+    def close(self):
         self.file_handle.close()
-        self._reset_handles()
+        self._unset_handles()
 
     def _get_item_by_name(self, name):
         assert name in self.archive_handle.getnames(), 'Archive does not contain requested filename: {}'.format(name) 
@@ -133,18 +131,19 @@ class ABC7ZFile(AbstractABC):
     def _isopen(self):
         return (self.file_handle is not None and
                 self.archive_handle is not None and
-                self._names_list is not None)
+                self._names is not None)
 
     def __getitem__(self, key):
         assert self._isopen()
         if isinstance(key, int):
-            name = self._names_list[key]
+            name = self._names[key]
             return self._get_item_by_name(name)
         elif isinstance(key, slice):
-            names = list(islice(self._names_list, key.start, key.stop, key.step))
+            names = list(islice(self._names, key.start, key.stop, key.step))
             return (self._get_item_by_name(name) for name in names)
+
     
-class ABCChunk(AbstractABC):
+class ABCChunk(AbstractABCDataHolder):
     """A chunk is a collection of files (with different modalities),
     that iterates over all files simultaneously."""
 
@@ -152,25 +151,25 @@ class ABCChunk(AbstractABC):
         self.filenames = filenames
         self.required_modalities = {_extract_modality(filename) for filename in filenames}
         self.file_handles = []
+        self.exitstack = ExitStack()
         self.load_chunk_to_memory = load_chunk_to_memory
-        self._reset_handles()       
+        self._unset_handles()       
 
-    def _reset_handles(self):
+    def _unset_handles(self):
         self.file_handles = None
-        
-    def _isopen(self):
-        return self.file_handles is not None
 
     def _open(self):
-        self.exitstack = ExitStack()
         self.file_handles = [self.exitstack.enter_context(ABC7ZFile(filename))
                                  for filename in self.filenames]
+        #self.archive_handle = [py7zlib.Archive7z(open(filename, "rb")) for filename in filenames]
+        #self._names = [archive.getnames() for archive in self.archive_handle] # rewrite with list and tree
 
-    def _close(self):
-        #for file_handle in self.file_handles:
-        #    file_handle.close()
+    def _isopen(self):
+        return (self.file_handles is not None)
+
+    def close(self):
         self.exitstack.close()
-        self._reset_handles()
+        self._unset_handles()
 
     def __iter__(self):
 
@@ -185,7 +184,6 @@ class ABCChunk(AbstractABC):
             # (this could be useful if the archives are mostly aligned, but contain broken items)
             iterables = zip(*self.file_handles)
 
-
         read_items_by_id = defaultdict(dict)  # maps item + modality into
         pathnames = defaultdict(dict) # pathnames for each modality
         archivenames = defaultdict(dict) # archive pathnames for each modality
@@ -199,7 +197,6 @@ class ABCChunk(AbstractABC):
 
             ids_to_yield = [item_id for item_id, read_modalities in read_items_by_id.items()
                             if not self.required_modalities - read_modalities.keys()]
-            
             for item_id in ids_to_yield:
                 read_modalities = read_items_by_id.pop(item_id)
                 read_pathnames = pathnames.pop(item_id)
@@ -210,7 +207,58 @@ class ABCChunk(AbstractABC):
                  
                 yield merged_item
 
-    
+    def __getitem__(self, key):
+#        assert self._isopen()
+        if isinstance(key, int):
+            file_handles = [file_handle[key] for file_handle in self.file_handles] 
+            iterables = zip(*file_handles)
+            read_items_by_id = defaultdict(dict)  # maps item + modality into
+            pathnames = defaultdict(dict) # pathnames for each modality
+            archivenames = defaultdict(dict) # archive pathnames for each modality
+            merged_items = []
+            for item, file in zip(file_handles, self.file_handles):
+                # TODO implement checks for raised exceptions during iterations
+                read_items_by_id[item.item_id][file.modality] = item
+                pathnames[item.item_id][file.modality] = item.pathname
+                archivenames[item.item_id][file.modality] = item.archive_pathname
+
+                ids_to_yield = [item_id for item_id, read_modalities in read_items_by_id.items()
+                            if not self.required_modalities - read_modalities.keys()]
+                merged_item = []
+                for item_id in ids_to_yield:
+                    read_modalities = read_items_by_id.pop(item_id)
+                    read_pathnames = pathnames.pop(item_id)
+                    read_achivenames = archivenames.pop(item_id)
+                    bytes_io = {modality: getattr(item, modality)
+                            for modality, item in read_modalities.items()}
+                    merged_item.append(ABCItem(pathname=read_pathnames, archive_pathname=read_achivenames, item_id=item_id, **bytes_io))
+            return merged_item
+
+        elif isinstance(key, slice):
+            file_handles = [file_handle[key.start:key.stop:key.step] for file_handle in self.file_handles]
+            iterables = zip(*file_handles)
+            read_items_by_id = defaultdict(dict)  # maps item + modality into
+            pathnames = defaultdict(dict) # pathnames for each modality
+            archivenames = defaultdict(dict) # archive pathnames for each modality
+            merged_item = []
+            for items in iterables:
+                for item, file in zip(items, self.file_handles):
+                    # TODO implement checks for raised exceptions during iterations
+                    read_items_by_id[item.item_id][file.modality] = item
+                    pathnames[item.item_id][file.modality] = item.pathname
+                    archivenames[item.item_id][file.modality] = item.archive_pathname
+
+                ids_to_yield = [item_id for item_id, read_modalities in read_items_by_id.items()
+                                if not self.required_modalities - read_modalities.keys()]
+                for item_id in ids_to_yield:
+                    read_modalities = read_items_by_id.pop(item_id)
+                    read_pathnames = pathnames.pop(item_id)
+                    read_achivenames = archivenames.pop(item_id)
+                    bytes_io = {modality: getattr(item, modality)
+                                for modality, item in read_modalities.items()}
+                    
+                    merged_item.append(ABCItem(pathname=read_pathnames, archive_pathname=read_achivenames, item_id=item_id, **bytes_io))
+            return merged_item
 
     def _preload(self):
         """Physically load entire chunk into working memory. Return tuple of iterables."""
@@ -259,19 +307,28 @@ from joblib import Parallel, delayed
 def read_slice(filename, slice_params):
     slice_start, slice_end = slice_params
     with ABC7ZFile(filename) as abc_7z_file:
-        x = abc_7z_file[slice_start:slice_end]
-        for i, item in enumerate(x):
-            y = item.obj.getvalue()
-            print(filename, slice_start + i, len(y))
+        x = abc_7z_file[slice_start]
+        print(x.obj.getvalue())
+        #x = abc_7z_file[slice_start:slice_end]
+        #for i, item in enumerate(x):
+        #    y = item.obj.getvalue()
+        #    print(y)
+        #    print(filename, slice_start + i, len(y))
 
-def read_chunk(filenames):
+def read_chunk(filenames, key1=0, key2=10):
     with ABCChunk(filenames) as chunk:
-        for item in chunk:
+        print('key test #1')
+        for item in chunk[key1]:
             print('obj:', len(item.obj.getvalue()))
             print('feat:', len(item.feat.getvalue()))
-            break 
-
-
+        print('key test #2')
+        for item in chunk[key2]:
+            print('obj:', len(item.obj.getvalue()))
+            print('feat:', len(item.feat.getvalue()))
+        print('key test #3')
+        for item in chunk[key1:key2]:
+            print('obj:', len(item.obj.getvalue()))
+            print('feat:', len(item.feat.getvalue()))
 
 if __name__ == '__main__':
     slice_start = list(range(0, 7000, 100))
