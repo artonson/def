2a21e8c2dc8b115cccea7e8d0de15dd87750c30c
Alexey Artemov <A.Artemov@skoltech.ru>
2019-10-01 15:07:16 +0300

    w.i.p. on data geneartion

--- scripts/dataset_utils/make_data.py
+++ scripts/dataset_utils/make_data.py
@@ -1,159 +1,282 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+import sys
+from io import BytesIO
 
-from sharpf.utils.shape import SequentialFilter, load_from_options
-from sharpf.data.abc_data import ABCData, ABCModality
-
+import trimesh
 from joblib import Parallel, delayed
+import numpy as np
+import yaml
+
+__dir__ = os.path.normpath(
+    os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), '..', '..')
+)
+sys.path[1:1] = [__dir__]
+
+from sharpf.data.abc_data import ABCModality, ABCChunk, ABC_7Z_FILEMASK, MergedABCItem
 
 
-# Method scheme:
-# 1) Obtain an archive filename as input.
-# 2) Locate the corresponding object and feature files.
-# 3) Filter the shapes using a number of filters, saving intermediate results.
-# 4) Crop patches from the shapes, saving to intermediate output files.
-# 5) Save dataset from the patches, saving to torch.FloatStorage .
+# the function for patch generator: breadth-first search
+
+def find_and_add(sets, desired_number_of_points, adjacency_graph):
+    counter = len(sets)  # counter for number of vertices added to the patch;
+    # sets is the list of vertices included to the patch
+    for verts in sets:
+        for vs in adjacency_graph.neighbors(verts):
+            if vs not in sets:
+                sets.append(vs)
+                counter += 1
+        #                 print(counter)
+        if counter >= desired_number_of_points:
+            break  # stop when the patch has more than 1024 vertices
+
+
+def trimesh_load(io: BytesIO):
+    """Read the mesh: since trimesh messes the indices, this has to be done manually."""
+
+    vertices, faces = [], []
+
+    for line in io:
+        values = line.strip().split()
+        if not values: continue
+        if values[0] == 'v':
+            vertices.append(np.array(values[1:4], dtype='float'))
+        elif values[0] == 'f':
+            faces.append(np.array([values[1].split('//')[0], values[2].split('//')[0], values[3].split('//')[0]],
+                                  dtype='int'))
+
+    vertices = np.array(vertices)
+    faces = np.array(faces) - 1
+
+    mesh = trimesh.base.Trimesh(vertices=vertices, faces=faces, process=False)  # create a mesh from the vertices
+    return mesh
+
+
+def geodesic_meshvertex_patches_from_item(item, n_points=1024):
+    """Sample patches from mesh, using vertices as points
+    in the point cloud,
+
+    :param item: MergedABCItem
+    :param n_points: number of points to output per patch (guaranteed)
+    :return:
+    """
+    yml = yaml.load(item.feat)
+    mesh = trimesh_load(item.obj)
+
+    sharp_idx = []
+    short_idx = []
+    for i in yml['curves']:
+        if len(i['vert_indices']) < 5:  # this is for filtering based on short curves:
+                                        # append all the vertices which are in the curves with less than 5 vertices
+            short_idx.append(np.array(i['vert_indices']) - 1)  # you need to substract 1 from vertex index,
+                                                               # since it starts with 1
+        if i.get('sharp') is True:
+            sharp_idx.append(np.array(i['vert_indices']) - 1)  # append all the vertices which are marked as sharp
+    if len(sharp_idx) > 0:
+        sharp_idx = np.unique(np.concatenate(sharp_idx))
+    if len(short_idx) > 0:
+        short_idx = np.unique(np.concatenate(short_idx))
+
+    surfaces = []
+    for i in yml['surfaces']:
+        if 'vert_indices' in i.keys():
+            surfaces.append(np.array(i['face_indices']) - 1)
+
+
+    sharp_indicator = np.zeros((len(mesh.vertices),))
+    sharp_indicator[sharp_idx] = 1
+
+    # and faces read previously
+    adjacency_graph = mesh.vertex_adjacency_graph
+
+    # select starting vertices to grow patches from,
+    # while iterating over them use BFS to generate patches
+    # TODO: why not sample densely / specify no. of patches to sample?
+    for j in np.linspace(0, len(mesh.vertices), 7, dtype='int')[:-1]:
+        set_of_verts = [j]
+        find_and_add(sets=set_of_verts, desired_number_of_points=n_points,
+                     adjacency_graph=adjacency_graph)  # BFS function
+
+        # TODO: what does this code do?
+        a = sharp_indicator[np.array(set_of_verts)[-100:]]
+        b = np.isin(np.array(set_of_verts)[-100:], np.array(set_of_verts)[-100:] - 1)
+        if (a[b].sum() > 3):
+            #                 print('here! border!',j)
+            continue
+
+        set_of_verts = np.unique(np.array(set_of_verts))  # the resulting list of vertices in the patch
+        # TODO why discard short lines?
+        if np.isin(set_of_verts, short_idx).any():  # discard a patch if there are short lines
+            continue
+
+        patch_vertices = mesh.vertices[set_of_verts]
+        patch_sharp = sharp_indicator[set_of_verts]
+        patch_normals = mesh.vertex_normals[set_of_verts]
+
+        if patch_sharp.sum() != 0:
+            sharp_rate.append(1)
+        else:
+            sharp_rate.append(0)
+
+        surfaces_numbers = []
+        if patch_vertices.shape[0] >= n_points:
+            # select those vertices, which are not sharp in order to use them for counting surfaces (sharp vertices
+            # are counted twice, since they are on the border between two surfaces, hence they are discarded)
+            appropriate_verts = set_of_verts[:n_points][
+                patch_sharp[:n_points].astype(int) == 0]
+
+            for surf_idx, surf_faces in enumerate(surfaces):
+                surf_verts = np.unique(mesh.faces[surf_faces].ravel())
+
+                if len(np.where(np.isin(appropriate_verts, surf_verts))[0]) > 0:
+                    surface_ratio = sharp_indicator[np.unique(np.array(surf_verts))].sum() / \
+                                    len(np.unique(np.array(surf_verts)))
+
+                    if surface_ratio > 0.6:
+                        break
+
+                    surfaces_numbers.append(surf_idx)  # write indices of surfaces which are present in the patch
+
+            if surface_ratio > 0.6:
+                continue
+
+            surface_rate.append(np.unique(np.array(surfaces_numbers)))
+            patch_vertices = patch_vertices[:n_points]
+            points.append(patch_vertices)
+            patch_vertices_normalized = patch_vertices - patch_vertices.mean(axis=0)
+            patch_vertices_normalized = patch_vertices_normalized / np.linalg.norm(patch_vertices_normalized,
+                                                                                   ord=2, axis=1).max()
+            points_normalized.append(patch_vertices_normalized)
+            patch_normals = patch_normals[:n_points]
+            normals.append(patch_normals)
+            labels.append(patch_sharp[:n_points])
+
+
+    return points, labels, normals, sharp_rate
+    points = []  # for storing initial coordinates of points
+    points_normalized = []  # for storing normalized coordinates of points
+    labels = []  # for storing 0-1 labels for non-sharp/sharp points
+    normals = []
+    surface_rate = []  # for counting how many surfaces are there in the patch
+    sharp_rate = []  # for indicator whether the patch contains sharp vertices at all
+    times = []  # for times (useless)
+    p_names = []  # for names of the patches in the format "initial_mesh_name_N", where N is the starting vertex index
+
+
+def euclidean_ball_patches_from_item():
+    pass
+
+
+PATCH_FUNC_BY_TYPE = {
+    'geodesic_meshvertex': geodesic_meshvertex_patches_from_item,
+    'euclidean_ball': euclidean_ball_patches_from_item,
+}
 
 
 @delayed
-def filter_meshes_worker(filter_fn, item):
-    item.archive_filename
-    item.name
+def generate_patches(meshes_filename, feats_filename, data_slice, n_points, patch_type, noise_type, noise_amount):
+    points = []  # for storing initial coordinates of points
+    points_normalized = []  # for storing normalized coordinates of points
+    labels = []  # for storing 0-1 labels for non-sharp/sharp points
+    normals = []
+    surface_rate = []  # for counting how many surfaces are there in the patch
+    sharp_rate = []  # for indicator whether the patch contains sharp vertices at all
+    times = []  # for times (useless)
+    p_names = []  # for names of the patches in the format "initial_mesh_name_N", where N is the starting vertex index
 
-    is_ok = filter_fn(item.obj)
-    return item.archive_filename, item.name, is_ok
+    patch_func = PATCH_FUNC_BY_TYPE[patch_type]
+
+    slice_start, slice_end = data_slice
+    with ABCChunk([meshes_filename, feats_filename]) as data_holder:
+        for item in data_holder[slice_start:slice_end]:
+            patches_points, patches_labels, patches_normals, patches_has_sharpness = patch_func(item, n_points)
 
 
 
-def filter_shapes(options):
-    """Filter the shapes using a number of filters, saving intermediate results."""
 
-    # create the required filter objects
-    sequential_filter = SequentialFilter([
-        load_from_options(filter_name, options.__dict__)
-        for filter_name in options.filters])
-
-    # create the data source iterator
-    abc_data = ABCData(options.data_dir, modalities=[ABCModality.FEAT],
-                       shape_representation='trimesh')
-
-    # run the filtering job in parallel
-    parallel = Parallel(n_jobs=options.n_jobs)
-    delayed_iterable = (filter_meshes_worker(sequential_filter, item) for item in abc_data)
-    output = parallel(delayed_iterable)
-
-    # write out the results
-    with open(options.output_filename, 'w') as output_file:
-        output_file.write('\n'.join([
-            '{} {}'.format(archive_filename, name)
-            for archive_filename, name, is_ok in output if is_ok
-        ]))
+    times = np.array(times)
+    p_names = np.array(p_names)
+    points = np.array(points)
+    points_normalized = np.array(points_normalized)
+    labels = np.array(labels).reshape(-1, 1024, 1)
+    normals = np.array(normals)
+    sharp_rate = np.array(sharp_rate)
+    return times, p_names, points, points_normalized, labels, sharp_rate, surface_rate, normals
 
 
 def make_patches(options):
-    pass
+    """Filter the shapes using a number of filters, saving intermediate results."""
 
+    # create the data source iterator
+    # abc_data = ABCData(options.data_dir,
+    #                    modalities=[ABCModality.FEAT.value, ABCModality.OBJ.value],
+    #                    chunks=[options.chunk],
+    #                    shape_representation='trimesh')
 
+    obj_filename = os.path.join(
+        options.input_dir,
+        ABC_7Z_FILEMASK.format(
+            chunk=options.chunk.zfill(4),
+            modality=ABCModality.OBJ.value,
+            version='00'
+        )
+    )
+    feat_filename = os.path.join(
+        options.input_dir,
+        ABC_7Z_FILEMASK.format(
+            chunk=options.chunk.zfill(4),
+            modality=ABCModality.FEAT.value,
+            version='00'
+        )
+    )
+    abc_data = ABCChunk([obj_filename, feat_filename])
 
-def make_torch_storage(options):
-    pass
+    processes_to_spawn = 10 * options.n_jobs
+    chunk_size = len(abc_data) // processes_to_spawn
+    abc_data_slices = [(start, start + chunk_size)
+                       for start in range(0, len(abc_data), chunk_size)]
+
+    # run the filtering job in parallel
+    parallel = Parallel(n_jobs=options.n_jobs)
+    delayed_iterable = (generate_patches(obj_filename, feat_filename,
+                                         data_slice,
+                                         options.num_points,
+                                         options.patch_type,
+                                         options.noise_type,
+                                         options.noise_amount)
+                        for data_slice in abc_data_slices)
+    output_by_slice = parallel(delayed_iterable)
+
+    # write out the results
+    output_filename = os.path.join(options.output_dir,'{}.txt'.format(options.chunk.zfill(4)))
+    with open(output_filename, 'w') as output_file:
+        output_file.write('\n'.join([
+            '{} {} {}'.format(pathname_by_modality['obj'], archive_pathname_by_modality['obj'], item_id)
+            for pathname_by_modality, archive_pathname_by_modality, item_id, is_ok in chain(*output_by_slice) if is_ok
+        ]))
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-j', '--jobs', type=int, default=4, help='CPU jobs to use in parallel [default: 4].')
-
-    subparsers = parser.add_subparsers(dest='command', help='sub-command help')
-
-    # create the parser for the "filter" command
-    stats_parser = subparsers.add_parser('filter', help='select meshes according to the specified filters.')
-    stats_parser.add_argument('--data-root', required=True, dest='data_root', help='root of the data tree (directory).')
-
-    stats_parser.add_argument('-f', '--filter', choices=[], action='append', dest='filters',
-                              help='the filters to use (can be multiple).')
-    stats_parser.add_argument('bar', type=int, help='bar help')
-    stats_parser.add_argument('bar', type=int, help='bar help')
-    stats_parser.add_argument('bar', type=int, help='bar help')
-    stats_parser.add_argument('bar', type=int, help='bar help')
-
-
-    # create the parser for the "patches" command
-    patches_parser = subparsers.add_parser('patches', help='generate patches ')
-    patches_parser.add_argument('bar', type=int, help='bar help')
-
-
-
-    # create the parser for the "dataset" command
-    dataset_parser = subparsers.add_parser('dataset', help='generate torch-based dataset from selected patches.')
-    dataset_parser.add_argument('bar', type=int, help='bar help')
-
-
-    parser.add_argument('-e', '--epochs', type=int, default=1, help='how many epochs to train [default: 1].')
-    parser.add_argument('-b', '--train-batch-size', type=int, default=128, dest='train_batch_size',
-                        help='train batch size [default: 128].')
-    parser.add_argument('-B', '--val-batch-size', type=int, default=128, dest='val_batch_size',
-                        help='val batch size [default: 128].')
-    parser.add_argument('--batches-before-val', type=int, default=1024, dest='batches_before_val',
-                        help='how many batches to train before validation [default: 1024].')
-    parser.add_argument('--batches-before-imglog', type=int, default=12, dest='batches_before_imglog',
-                        help='log images each batches-before-imglog validation batches [default: 12].')
-    parser.add_argument('--mini-val-batches-n-per-subset', type=int, default=12, dest='mini_val_batches_n_per_subset',
-                        help='how many batches per subset to run for mini validation [default: 12].')
-
-    parser.add_argument('--model-spec', dest='model_spec_filename', required=True,
-                        help='model specification JSON file to use [default: none].')
-    parser.add_argument('--infer-from-spec', dest='infer_from_spec', action='store_true', default=False,
-                        help='if set, --model, --save-model-file, --logging-file, --tboard-json-logging-file,'
-                             'and --tboard-dir are formed automatically [default: False].')
-    parser.add_argument('--log-dir-prefix', dest='logs_dir', default='/logs',
-                        help='path to root of logging location [default: /logs].')
-    parser.add_argument('-m', '--init-model-file', dest='init_model_filename',
-                        help='Path to initializer model file [default: none].')
-
-    parser.add_argument('-s', '--save-model-file', dest='save_model_filename',
-                        help='Path to output vectorization model file [default: none].')
-    parser.add_argument('--batches_before_save', type=int, default=1024, dest='batches_before_save',
-                        help='how many batches to run before saving the model [default: 1024].')
-
-    parser.add_argument('--data-root', required=True, dest='data_root', help='root of the data tree (directory).')
-    parser.add_argument('--data-type', required=True, dest='dataloader_type',
-                        help='type of the train/val data to use.', choices=dataloading.prepare_loaders.keys())
-    parser.add_argument('--handcrafted-train', required=False, action='append',
-                        dest='handcrafted_train_paths', help='dirnames of handcrafted datasets used for training '
-                                                             '(sought for in preprocessed/synthetic_handcrafted).')
-    parser.add_argument('--handcrafted-val', required=False, action='append',
-                        dest='handcrafted_val_paths', help='dirnames of handcrafted datasets used for validation '
-                                                           '(sought for in preprocessed/synthetic_handcrafted).')
-    parser.add_argument('--handcrafted-val-part', required=False, type=float, default=.1,
-                        dest='handcrafted_val_part', help='portion of handcrafted_train used for validation')
-    parser.add_argument('-M', '--memory-constraint', required=True, type=int, dest='memory_constraint',help='maximum RAM usage in bytes.')
-
-    parser.add_argument('-r', '--render-resolution', dest='render_res', default=64, type=int,
-                        help='resolution used for rendering.')
-
-    parser.add_argument('--verbose', action='store_true', default=False, dest='verbose',
-                        help='verbose output [default: False].')
-    parser.add_argument('-l', '--logging-file', dest='logging_filename',
-                        help='Path to output logging text file [default: output to stdout only].')
-    parser.add_argument('-tl', '--tboard-json-logging-file', dest='tboard_json_logging_file',
-                        help='Path to output logging JSON file with scalars [default: none].')
-    parser.add_argument('-x', '--tboard-dir', dest='tboard_dir',
-                        help='Path to tensorboard [default: do not log events].')
-    parser.add_argument('-w', '--overwrite', action='store_true', default=False,
-                        help='If set, overwrite existing logs [default: exit if output dir exists].')
+    parser.add_argument('-j', '--jobs', dest='n_jobs',
+                        type=int, default=4, help='CPU jobs to use in parallel [default: 4].')
+    parser.add_argument('-i', '--input-dir', dest='input_dir',
+                        required=True, help='input dir with ABC dataset.')
+    parser.add_argument('-c', '--chunk', required=True, help='ABC chunk id to process.')
+    parser.add_argument('-o', '--output-dir', dest='output_dir',
+                        required=True, help='output dir.')
+    parser.add_argument('-g', '--filter-config', dest='filter_config',
+                        required=True, help='filter configuration file.')
+    parser.add_argument('-n', '--num-points', dest='num_points', type=int,
+                        required=True, help='number of points to ')
 
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     options = parse_args()
-
-    function_to_run = {
-        'filter': filter_shapes,
-        'patch': make_patches,
-        'dataset': make_torch_storage,
-    }[options.command]
-
-    function_to_run(options)
+    make_patches(options)
---
+++ scripts/slurm/make_patches.sbatch.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#SBATCH --job-name=sharpf_dataset_filter
+#SBATCH --output=array_%A_%a.out
+#SBATCH --error=array_%A_%a.err
+#SBATCH --array=0-9
+#SBATCH --time=01:00:00
+#SBATCH --partition=cpu_big
+#SBATCH --cpus-per-task=24
+#SBATCH --ntasks-per-node=1
+
+set -x
+CPUS_PER_TASK=24
+
+SIMAGES_DIR=/gpfs/gpfs0/3ddl/singularity-images
+IMAGE_NAME="artonson/sharp_features"
+IMAGE_VERSION="latest"
+IMAGE_NAME_TAG="${IMAGE_NAME}:${IMAGE_VERSION}"
+SIMAGE_FILENAME="${SIMAGES_DIR}/$(echo ${IMAGE_NAME_TAG} | tr /: _).sif"
+
+HOST_CODE_DIR="/trinity/home/a.artemov/repos/sharp_features"
+HOST_DATA_DIR="/gpfs/gpfs0/3ddl/datasets/abc"
+HOST_OUT_DIR="/gpfs/gpfs0/3ddl/sharp_features/data"
+HOST_LOG_DIR="/logs"
+
+CONT_CODE_DIR="/code"
+CONT_DATA_DIR="/data"
+CONT_OUT_DIR="/out"
+CONT_LOG_DIR="/logs"
+
+echo SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID}
+
+module load apps/singularity-3.2.0
+singularity exec \
+  --bind ${HOST_CODE_DIR}:${CONT_CODE_DIR} \
+  --bind ${HOST_DATA_DIR}:${CONT_DATA_DIR} \
+  --bind ${HOST_LOG_DIR}:${CONT_LOG_DIR} \
+  --bind ${HOST_OUT_DIR}:${CONT_OUT_DIR} \
+  ${SIMAGE_FILENAME} \
+  python3 ${CONT_CODE_DIR}/scripts/dataset_utils/dataset_filter.py \
+    --input-dir ${CONT_DATA_DIR} \
+    --chunk ${SLURM_ARRAY_TASK_ID} \
+    --output-dir ${CONT_OUT_DIR} \
+    --jobs ${CPUS_PER_TASK} \
+    --filter-config ${CONT_CODE_DIR}/scripts/dataset_utils/dataset_filter_config.json
+
