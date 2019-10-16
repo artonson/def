f5f9f547b9111a186ce9410c6a820cb649f2fcb4
Gleb Bobrovskikh <bobrovskihg@gmail.com>
2019-10-16 14:00:43 +0300

    fixed reading data problem

--- CGAL/ear_scripts/code/read_data_ear.py
+++ CGAL/ear_scripts/code/read_data_ear.py
@@ -26,19 +26,24 @@ if __name__ == '__main__':
                 if i <= 4:
                     continue
                 print('datafile #{}'.format(i))
-                tmp_f = open(out_directory+'/'+'ear_tmp_{}.xyz'.format(i), 'w')
+                tmp_f = open('ear_tmp_{}.xyz'.format(i), 'w')
                 for point in data:
                     tmp_f.write('{} {} {}\n'.format(point[0], point[1], point[-1]))
                 tmp_filename = 'ear_tmp_{}.xyz'.format(i)
                 tmp_f.close()
-                exec_cmd = ' '.join(['./edge_aware -filename', tmp_filename, '-o', out_directory])
+                exec_cmd = ' '.join(['./edge_aware -filename', tmp_filename])
                 os.system(exec_cmd)
+            #temp_f = open('points_classification_' + tmp_filename.split('.')[0] + '.txt')
+            #classification[i] = np.array(list(map(int, temp_f.readline().split(' ')[:-1])))[:, np.newaxis]
+                print(os.listdir('.'))
                 exec_cmd = 'mv points_2.txt {}/{}_points_2.txt'.format(out_directory, i)
                 os.system(exec_cmd)
                 exec_cmd = 'mv normals_2.txt {}/{}_normals_2.txt'.format(out_directory, i)
                 os.system(exec_cmd)
                 exec_cmd = 'mv sharp_classif.txt {}/{}_sharp_classif.txt'.format(out_directory, i)
                 os.system(exec_cmd)
-                os.remove(out_directory+'/'+tmp_filename)
+                os.remove(tmp_filename)
                 
+                if i >= 8:
+                    break
 
