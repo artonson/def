3a628812f4f56a3cf66917df097b1728789e5fa2
Alexey Artemov <a.artemov@skoltech.ru>
2019-11-01 00:01:15 +0300

    * update saving output data to hdf5 format * fix the script to run jobs in parallel * update h5py version >= 2.10 * select more adequate poisson_disk config

--- scripts/dataset_utils/make_data.py
+++ scripts/dataset_utils/make_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 from copy import deepcopy
 import os
 import sys
@@ -21,6 +22,7 @@ from sharpf.data.annotation import ANNOTATOR_BY_TYPE
 from sharpf.data.mesh_nbhoods import NBHOOD_BY_TYPE
 from sharpf.data.noisers import NOISE_BY_TYPE
 from sharpf.data.point_samplers import SAMPLER_BY_TYPE
+from sharpf.utils.common import eprint
 from sharpf.utils.mesh_utils import trimesh_load
 
 
@@ -61,38 +63,44 @@ def generate_patches(meshes_filename, feats_filename, data_slice, config, output
         point_patches = []
         for item in data_holder[slice_start:slice_end]:
             # load the mesh and the feature curves annotations
-            mesh = trimesh_load(item.obj)
-            features = yaml.load(item.feat, Loader=yaml.Loader)
+            try:
+                mesh = trimesh_load(item.obj)
+                features = yaml.load(item.feat, Loader=yaml.Loader)
 
-            # index the mesh using a neighbourhood functions class
-            # (this internally may call indexing, so for repeated invocation one passes the mesh)
-            nbhood_extractor.index(mesh)
+                # index the mesh using a neighbourhood functions class
+                # (this internally may call indexing, so for repeated invocation one passes the mesh)
+                nbhood_extractor.index(mesh)
 
-            for patch_idx in range(n_patches_per_mesh):
-                # extract neighbourhood
-                nbhood, orig_vert_indices, orig_face_indexes = nbhood_extractor.get_nbhood()
+                for patch_idx in range(n_patches_per_mesh):
+                    # extract neighbourhood
+                    nbhood, orig_vert_indices, orig_face_indexes = nbhood_extractor.get_nbhood()
 
-                # sample the neighbourhood to form a point patch
-                points, normals = sampler.sample(nbhood)
+                    # sample the neighbourhood to form a point patch
+                    points, normals = sampler.sample(nbhood)
 
-                # create a noisy sample
-                noisy_points = noiser.make_noise(points, normals)
+                    # create a noisy sample
+                    noisy_points = noiser.make_noise(points, normals)
 
-                # create annotations: condition the features onto the nbhood, then compute the TSharpDF
-                nbhood_features = compute_curves_nbhood(features, orig_vert_indices, orig_face_indexes)
-                distances, directions = annotator.annotate(nbhood, nbhood_features, noisy_points)
+                    # create annotations: condition the features onto the nbhood, then compute the TSharpDF
+                    nbhood_features = compute_curves_nbhood(features, orig_vert_indices, orig_face_indexes)
+                    distances, directions = annotator.annotate(nbhood, nbhood_features, noisy_points)
 
-                has_sharp = any(curve['sharp'] for curve in nbhood_features['curves'])
-                patch_info = {
-                    'points': noisy_points,
-                    'distances': distances,
-                    'directions': directions,
-                    'item_id': item.item_id,
-                    'orig_vert_indices': orig_vert_indices,
-                    'orig_face_indexes': orig_face_indexes,
-                    'has_sharp': has_sharp
-                }
-                point_patches.append(patch_info)
+                    has_sharp = any(curve['sharp'] for curve in nbhood_features['curves'])
+                    patch_info = {
+                        'points': noisy_points,
+                        'distances': distances,
+                        'directions': directions,
+                        'item_id': item.item_id,
+                        'orig_vert_indices': orig_vert_indices,
+                        'orig_face_indexes': orig_face_indexes,
+                        'has_sharp': has_sharp
+                    }
+                    point_patches.append(patch_info)
+
+            except Exception as e:
+                eprint('Error processing item {item_id} from chunk {chunk}: {what}'.format(
+                    item_id=item.item_id, chunk='[{},{}]'.format(meshes_filename, feats_filename), what=e
+                ))
 
     with h5py.File(output_file, 'w') as hdf5file:
         points = np.stack(patch['points'] for patch in point_patches)
@@ -105,13 +113,21 @@ def generate_patches(meshes_filename, feats_filename, data_slice, config, output
         hdf5file.create_dataset('directions', data=directions, dtype=np.float64)
 
         item_ids = [patch['item_id'] for patch in point_patches]
-        hdf5file.create_dataset('item_id', data=item_ids, dtype=h5py.string_dtype(encoding='ascii'))
+        hdf5file.create_dataset('item_id', data=np.string_(item_ids), dtype=h5py.string_dtype(encoding='ascii'))
 
         orig_vert_indices = [patch['orig_vert_indices'].astype('int32') for patch in point_patches]
-        hdf5file.create_dataset('orig_vert_indices', data=orig_vert_indices, dtype=h5py.vlen_dtype(np.dtype('int32')))
+        vert_dataset = hdf5file.create_dataset('orig_vert_indices',
+                                               shape=(len(orig_vert_indices),),
+                                               dtype=h5py.special_dtype(vlen=np.int32))
+        for i, vert_indices in enumerate(orig_vert_indices):
+            vert_dataset[i] = vert_indices
 
         orig_face_indexes = [patch['orig_face_indexes'].astype('int32') for patch in point_patches]
-        hdf5file.create_dataset('orig_face_indexes', data=orig_face_indexes, dtype=h5py.vlen_dtype(np.dtype('int32')))
+        face_dataset = hdf5file.create_dataset('orig_face_indexes',
+                                               shape=(len(orig_face_indexes),),
+                                               dtype=h5py.special_dtype(vlen=np.int32))
+        for i, face_indices in enumerate(orig_face_indexes):
+            face_dataset[i] = face_indices.flatten()
 
         has_sharp = np.stack(patch['has_sharp'] for patch in point_patches).astype(bool)
         hdf5file.create_dataset('has_sharp', data=has_sharp, dtype=np.bool)
@@ -144,29 +160,26 @@ def make_patches(options):
     )
     abc_data = ABCChunk([obj_filename, feat_filename])
 
+    with open(options.dataset_config) as config_file:
+        config = json.load(config_file)
+
     processes_to_spawn = 10 * options.n_jobs
     chunk_size = len(abc_data) // processes_to_spawn
     abc_data_slices = [(start, start + chunk_size)
                        for start in range(0, len(abc_data), chunk_size)]
+    output_files = [
+        os.path.join(
+            options.output_dir,
+            'abc_{chunk}_{slice}.hdf5'.format(chunk=options.chunk.zfill(4),
+                                         slice='_'.join(data_slice))
+        )
+        for data_slice in abc_data_slices]
 
     # run the filtering job in parallel
     parallel = Parallel(n_jobs=options.n_jobs)
-    delayed_iterable = (generate_patches(obj_filename, feat_filename,
-                                         data_slice,
-                                         options.num_points,
-                                         options.patch_type,
-                                         options.noise_type,
-                                         options.noise_amount)
-                        for data_slice in abc_data_slices)
-    output_by_slice = parallel(delayed_iterable)
-
-    # write out the results
-    output_filename = os.path.join(options.output_dir,'{}.txt'.format(options.chunk.zfill(4)))
-    with open(output_filename, 'w') as output_file:
-        output_file.write('\n'.join([
-            '{} {} {}'.format(pathname_by_modality['obj'], archive_pathname_by_modality['obj'], item_id)
-            for pathname_by_modality, archive_pathname_by_modality, item_id, is_ok in chain(*output_by_slice) if is_ok
-        ]))
+    delayed_iterable = (generate_patches(obj_filename, feat_filename, data_slice, config, out_filename)
+                        for data_slice, out_filename in zip(abc_data_slices, output_files))
+    parallel(delayed_iterable)
 
 
 def parse_args():
@@ -179,10 +192,8 @@ def parse_args():
     parser.add_argument('-c', '--chunk', required=True, help='ABC chunk id to process.')
     parser.add_argument('-o', '--output-dir', dest='output_dir',
                         required=True, help='output dir.')
-    parser.add_argument('-g', '--filter-config', dest='filter_config',
-                        required=True, help='filter configuration file.')
-    parser.add_argument('-n', '--num-points', dest='num_points', type=int,
-                        required=True, help='number of points to ')
+    parser.add_argument('-g', '--dataset-config', dest='dataset_config',
+                        required=True, help='dataset configuration file.')
 
     return parser.parse_args()
 
