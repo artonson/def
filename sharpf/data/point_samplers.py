2f945377dec5af293180e3f9d150a7eee5edeba7
Alexey Artemov <a.artemov@skoltech.ru>
2019-10-31 15:33:41 +0300

    fix choosing which points to return

--- sharpf/data/point_samplers.py
+++ sharpf/data/point_samplers.py
@@ -61,7 +61,7 @@ class PoissonDiskSampler(SamplerFunc):
             radius=self.poisson_disk_radius, use_geodesic_distance=True)
 
         # ensure that we are returning exactly n_points
-        return_idx = np.random.choice(np.arange(), size=self.n_points, replace=False)
+        return_idx = np.random.choice(np.arange(len(points)), size=self.n_points, replace=False)
         return points[return_idx], normals[return_idx]
 
     @classmethod
