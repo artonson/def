8bcb3c6ac10997454ee791803589f91947913dd1
Alexey Artemov <a.artemov@skoltech.ru>
2019-11-11 23:57:38 +0300

    add train script to run on slurm at zhores

---
+++ env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# obligatory docker/singularity container namings
+export PROJECT="sharp_features"
+export SIMAGES_DIR=/gpfs/gpfs0/3ddl/singularity-images
+export IMAGE_NAME="artonson/${PROJECT}"
+export IMAGE_VERSION="latest"
+export IMAGE_NAME_TAG="${IMAGE_NAME}:${IMAGE_VERSION}"
+export SIMAGE_FILENAME="${SIMAGES_DIR}/$(echo ${IMAGE_NAME_TAG} | tr /: _).sif"
+
