1d57be417b426d547e76251de2d302bcf978b1e2
Alexey Artemov <a.artemov@skoltech.ru>
2019-11-12 01:12:42 +0300

    change image version in env to pointweb-ops

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@
 export PROJECT="sharp_features"
 export SIMAGES_DIR=/gpfs/gpfs0/3ddl/singularity-images
 export IMAGE_NAME="artonson/${PROJECT}"
-export IMAGE_VERSION="latest"
+export IMAGE_VERSION="pointweb-ops"
 export IMAGE_NAME_TAG="${IMAGE_NAME}:${IMAGE_VERSION}"
 export SIMAGE_FILENAME="${SIMAGES_DIR}/$(echo ${IMAGE_NAME_TAG} | tr /: _).sif"
 
