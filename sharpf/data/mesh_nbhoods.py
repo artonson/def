66e00b76c2123a493c686ba98a9298c2614a553a
albmatveev <31800984+albmatveev@users.noreply.github.com>
2019-12-02 12:15:20 +0300

    Fix NaNs, envelope radius, indexing, border sharp

--- sharpf/data/mesh_nbhoods.py
+++ sharpf/data/mesh_nbhoods.py
@@ -53,8 +53,9 @@ class EuclideanSphere(NeighbourhoodFunc):
 
     def get_nbhood(self):
         # select vertices falling within euclidean sphere
+        radius_scaler = self.mesh.edges_unique_length.mean()
         _, vert_indices = self.tree.query(
-            self.centroid, k=self.n_vertices, distance_upper_bound=self.radius)
+            self.centroid, k=self.n_vertices, distance_upper_bound=self.radius * radius_scaler)
 
         # get all faces that share vertices with selected vertices
         vert_indices = vert_indices[vert_indices < len(self.mesh.vertices)]
@@ -78,7 +79,7 @@ class EuclideanSphere(NeighbourhoodFunc):
             process=False,
             validate=False)
 
-        return neighbourhood, adj_vert_indices, self.mesh.faces[adj_face_indexes]
+        return neighbourhood, adj_vert_indices, self.mesh.faces[adj_face_indexes], radius_scaler
 
     @classmethod
     def from_config(cls, config):
@@ -91,9 +92,10 @@ class RandomEuclideanSphere(EuclideanSphere):
         self.radius_delta = radius_delta
 
     def get_nbhood(self):
+        radius_scaler = self.mesh.edges_unique_length.mean()
         centroid_idx = np.random.choice(len(self.mesh.vertices))
         self.centroid = self.mesh.vertices[centroid_idx]
-        self.radius = np.random.uniform(
+        self.radius = radius_scaler * np.random.uniform(
             self.radius - self.radius_delta,
             self.radius + self.radius_delta)
         return super(RandomEuclideanSphere, self).get_nbhood()
