000e39e6a87a817b3d2bb7b9f17241fd64dc7074
Alexey Artemov <a.artemov@skoltech.ru>
2020-02-20 18:28:13 +0300

    add eprint with time

--- sharpf/utils/common.py
+++ sharpf/utils/common.py
@@ -1,9 +1,16 @@
 import os
 import sys
+from datetime import datetime
 
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+
+def eprint_t(*args, **kwargs):
+    ts_fmt = datetime.now().strftime("%d.%m.%Y %H:%M:%S.%f ")
+    eargs = (ts_fmt, ) + args
+    eprint(eargs, **kwargs)
 
 
 def change_ext(filename, new_ext):
