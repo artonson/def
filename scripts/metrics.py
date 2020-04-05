d94be37091beafdc9c99b42d75161e302f30eece
Alexey Artemov <A.Artemov@skoltech.ru>
2020-04-06 01:55:43 +0300

    bugfix

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -37,7 +37,7 @@ import h5py
 
 __dir__ = os.path.normpath(
     os.path.join(
-        os.path.dirname(os.path.realpath(__file__)), '..', '..')
+        os.path.dirname(os.path.realpath(__file__)), '..')
 )
 sys.path[1:1] = [__dir__]
 
