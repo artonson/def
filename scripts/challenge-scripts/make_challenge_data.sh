da347500140ad6ed22014f8f69bbdffd1dac5263
Alexey Artemov <a.artemov@skoltech.ru>
2020-05-11 18:00:02 +0300

    fix utilities.py:read_file to produce the correct 1d distance arrays; add code for make_challenge_data.sh to create all regression-based data at once

--- scripts/challenge-scripts/make_challenge_data.sh
+++ scripts/challenge-scripts/make_challenge_data.sh
@@ -9,7 +9,7 @@ set -x
 DATASETS_DIR=/mnt/neuro/artonson/sharp_features_datasets
 MODALITY=points
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../.. >/dev/null 2>&1 && pwd )"
-OUTPUT_DIR=/home/artonson/tmp
+OUTPUT_DIR=/home/artonson/datasets/cvpr_abc_challenge
 
 
 hdf5_to_txt() {
@@ -20,17 +20,44 @@ hdf5_to_txt() {
     local I1=$5
     local I2=$6
     local OUTPUT_BASE_DIR=$7
+    local FORMAT=$8
+    local SPLIT_XY=$9
+    local OUTPUT_THR=${10}
+
+    if [[ ${SPLIT_XY} ]] ; then
+        SPLIT_ARG="--split-xy"
+    fi
+
+    if [[ ${OUTPUT_THR} ]] ; then
+        PREFIX="segmentation_"
+        OUTPUT_THR_ARG="--target-thr ${OUTPUT_THR}"
+    else
+        PREFIX="field_"
+    fi
 
     for FILE_IDX in $( seq ${I1} ${I2} )
     do
-        local OUTPUT_DIR="${OUTPUT_BASE_DIR}/${SPLIT}_${FILE_IDX}"
         local DATASET="${DATASETS_DIR}/${MODALITY}/dataset_config_${RES}_res_clean.json/${SPLIT}/${SPLIT}_${FILE_IDX}.hdf5"
+        local OUTPUT_DIR="${OUTPUT_BASE_DIR}/${SPLIT}_${FILE_IDX}"
 
         ./convert_hdf5_to_txts.py \
-            -i ${DATASET} \
-            -o ${OUTPUT_DIR} \
-            -d points \
-            -t distances
+            --input-file ${DATASET} \
+            --output-dir ${OUTPUT_DIR} \
+            --data-label points \
+            --target-label distances \
+            --output-format ${FORMAT} \
+            --output-prefix ${PREFIX} \
+            ${SPLIT_ARG} \
+            ${OUTPUT_THR_ARG}
+
+        if [[ ${FORMAT} == "npy" ]]; then
+            if [[ ${SPLIT_XY} ]]; then
+                mv "${PREFIX}000000_data.npy" "${PREFIX}${FILE_IDX}_data.npy"
+                mv "${PREFIX}000000_target.npy" "${PREFIX}${FILE_IDX}_target.npy"
+            else
+                mv "${PREFIX}000000.npy" "${PREFIX}${FILE_IDX}.npy"
+            fi
+        fi
 
         local TARGZ_FILENAME="${RES}_res_${SPLIT}_${FILE_IDX}.tar.gz"
         tar -zcf ${OUTPUT_BASE_DIR}/${TARGZ_FILENAME} ${OUTPUT_DIR}
@@ -39,15 +66,31 @@ hdf5_to_txt() {
     done
 }
 
+MAX_TRAIN_IDX=5
+MAX_VAL_IDX=1
+MAX_TEST_IDX=1
 
+OUTPUT_DIR_TXT=${OUTPUT_DIR}/txt
+# text files
 for RES in high med low
 do
     # train files
-    # hdf5_to_txt ${DATASETS_DIR} ${MODALITY} ${RES} train 0 0 ${OUTPUT_DIR}
-
+    hdf5_to_txt ${DATASETS_DIR} ${MODALITY} ${RES} train 0 ${MAX_TRAIN_IDX} ${OUTPUT_DIR_TXT} txt false
     # val files
-    hdf5_to_txt ${DATASETS_DIR} ${MODALITY} ${RES} val 0 0 ${OUTPUT_DIR}
-
+    hdf5_to_txt ${DATASETS_DIR} ${MODALITY} ${RES} val 0 ${MAX_VAL_IDX} ${OUTPUT_DIR_TXT} txt true
     # test files
-    hdf5_to_txt ${DATASETS_DIR} ${MODALITY} ${RES} test 0 0 ${OUTPUT_DIR}
+    hdf5_to_txt ${DATASETS_DIR} ${MODALITY} ${RES} test 0 ${MAX_TEST_IDX} ${OUTPUT_DIR} txt true
+done
+
+
+OUTPUT_DIR_NPY=${OUTPUT_DIR}/npy
+# NPY files
+for RES in high med low
+do
+    # train files
+    hdf5_to_txt ${DATASETS_DIR} ${MODALITY} ${RES} train 0 ${MAX_TRAIN_IDX} ${OUTPUT_DIR_NPY} npy false
+    # val files
+    hdf5_to_txt ${DATASETS_DIR} ${MODALITY} ${RES} val 0 ${MAX_VAL_IDX} ${OUTPUT_DIR_NPY} npy true
+    # test files
+    hdf5_to_txt ${DATASETS_DIR} ${MODALITY} ${RES} test 0 ${MAX_TEST_IDX} ${OUTPUT_DIR_NPY} npy true
 done
--- scripts/challenge-scripts/utilities.py
+++ scripts/challenge-scripts/utilities.py
@@ -2,13 +2,13 @@ import numpy as np
 
 
 def read_file(path, read_points=True, read_distances=True):
-    """Read txt file and return numpy arrays for points and distance values
+    """Read txt file and return numpy arrays for points (n_points, 3) and distance (n_points, ) values
 
     # Read points and normals
     >>> points, distances = read_file("distance_07102.txt")
 
     # Read points only:
-    >>> points, _ = read_file("distance_07102.txt", read_distances=False)
+    >>> points, _ = read_file("distance_07102_data.txt", read_distances=False)
     """
 
     assert read_points or read_distances, 'at least one of read_points or read_distances must be specified'
@@ -16,20 +16,20 @@ def read_file(path, read_points=True, read_distances=True):
     data_target = np.loadtxt(path)
 
     if read_points and read_distances:
-        data, target = data_target[:, :3], data_target[:, 3:]
+        data, target = data_target[:, :3], np.squeeze(data_target[:, 3:])
 
     elif read_points:  # and not read_distances
         data, target = data_target[:, :3], None
 
     else:  # not read_points and read_distances
-        data, target = None, data_target[:, 3:]
+        data, target = None, data_target
 
     return data, target
 
 
 def mean_error_field(ref, est):
     """Calculates the mean errors between reference distance field and estimated distance field."""
-    assert len(est.shape) == len(ref.shape) == 1, "1d arrays expected"
+    assert len(est.shape) == len(ref.shape) == 1, '1d arrays expected'
 
     # make sure that estimations live in [0, 1]
     est = np.maximum(0, np.minimum(1, est))
@@ -41,7 +41,7 @@ def mean_error_field(ref, est):
 
 def mean_error_(ref, est):
     """Calculates the mean errors between reference distance field and estimated distance field."""
-    assert len(est.shape) == len(ref.shape) == 1, "1d arrays expected"
+    assert len(est.shape) == len(ref.shape) == 1, '1d arrays expected'
 
     # make sure that estimations live in [0, 1]
     est = np.maximum(0, np.minimum(1, est))
