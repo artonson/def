cfd9db89b2d14459a21afda81549d088eee9dcc4
Alexey Artemov <a.artemov@skoltech.ru>
2020-05-11 16:34:22 +0300

    add utilities for challenge; add --target-thr, --split-xy, --output-format options to select what and how will be saved

--- scripts/challenge-scripts/convert_hdf5_to_txts.py
+++ scripts/challenge-scripts/convert_hdf5_to_txts.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from functools import partial
 import os
 import sys
 
@@ -39,33 +40,75 @@ def main(options):
         num_workers=4
     )
 
+    filename_template = '{prefix}{index:06d}{suffix}.{fmt}'
+
+    items = []  # used only for 'npy' format
     from tqdm import tqdm
     for item_idx, item in tqdm(enumerate(loader)):
         data = item[options.data_label].squeeze().numpy()
         target = item[options.target_label].squeeze().numpy()
-        X = np.column_stack((data, target))
+        if None is not options.target_thr:
+            target = np.array(target < options.target_thr, dtype=np.float)
 
-        output_filename = os.path.join(
-            options.output_dir,
-            '{prefix}{index:05d}.txt'.format(
+        if options.output_format == 'txt':
+            # save separate txt files
+            output_filename = partial(
+                filename_template.format,
                 prefix=options.output_prefix,
-                index=item_idx
+                index=item_idx,
+                fmt='txt'
             )
+            if options.split_xy:
+                np.savetxt(output_filename(suffix='_data'), data, fmt='%.10e')
+                np.savetxt(output_filename(suffix='_target'), target, fmt='%.10e')
+            else:
+                X = np.column_stack((data, target))
+                np.savetxt(output_filename(suffix=''), X, fmt='%.10e')
+
+        else:
+            assert options.output_format == 'npy', 'weird output format {}'.format(options.output_format)
+            items.append((data, target))
+
+    if options.output_format == 'npy':
+        output_filename = partial(
+            filename_template.format,
+            prefix=options.output_prefix,
+            index=0,
+            fmt='npy'
         )
-        np.savetxt(output_filename, X, fmt='%.10e')
+        if options.split_xy:
+            data_items = np.stack([data for data, _ in items])
+            np.save(output_filename(suffix='_data'), data_items)
+
+            target_items = np.stack([target for _, target in items])
+            np.save(output_filename(suffix='_target'), target_items)
+        else:
+            X = np.stack([
+                np.column_stack((data, target))
+                for data, target in items])
+            np.save(output_filename(suffix=''), X)
 
 
 def parse_options():
     parser = argparse.ArgumentParser(
-        description='Convert HDF5 file to a set of TXT files.')
+        description='Convert HDF5 file to a set of TXT/NPY files.')
 
     parser.add_argument('-i', '--input-file', dest='hdf5_input_file', help='HDF5 input files.')
     parser.add_argument('-o', '--output-dir', dest='output_dir', help='directory for output files.')
 
     parser.add_argument('-d', '--data-label', dest='data_label', required=True, help='data label.')
     parser.add_argument('-t', '--target-label', dest='target_label', required=True, help='target label.')
+    parser.add_argument('-T', '--target-thr', dest='target_thr', type=float,
+                        required=False, help='if specified, targets with values smaller than T will be set to '
+                                             'binary 1 (sharp), and those with values greater than T to binary 0.')
 
-    parser.add_argument('--output-prefix', dest='output_prefix', default='distance_',
+    parser.add_argument('-s', '--split-xy', dest='split_xy', action='store_true',
+                        default=False, help='if set, data and target will be extracted as separate '
+                                            'files (with "_data" suffix for data and "_target" suffix for target '
+                                            '(useful for creating test/validation files).')
+    parser.add_argument('-f', '--output-format', dest='output_format', default='txt',
+                        choices=['txt', 'npy'], help='output format to use.')
+    parser.add_argument('-p', '--output-prefix', dest='output_prefix', default='distance_',
                         help='string prefix of output filename to use.')
 
     parser.add_argument('-w', '--overwrite', dest='overwrite', action='store_true',
