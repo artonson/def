changed balanced accuracy computation, changed dict keys in depth ds, fixed cfgs

sharpf/modeling/meta_arch/depth_regressor.py
@@ -27,15 +27,9 @@ class DepthRegressor(LightningModule):
 
     def __init__(self, cfg):
         super().__init__()
-<<<<<<< HEAD
-        self.hparams = flatten_omegaconf(cfg)  # there should be better official way later
-        self.cfg = cfg
+        self.hparams = cfg  # there should be better official way later
         self.task = 'regression'
-        self.model = build_model(cfg.model)
-=======
-        self.hparams = cfg
         self.model = build_model(self.hparams.model)
->>>>>>> pl_hydra
         self.example_input_array = torch.rand(1, 1, 64, 64)
         self.data_dir = hydra.utils.to_absolute_path(self.hparams.data.data_dir)
 
@@ -50,7 +44,7 @@ class DepthRegressor(LightningModule):
         return self.model(x)
 
     def training_step(self, batch, batch_idx):
-        points, distances = batch['image'], batch['distances']
+        points, distances = batch['image'], batch['distance_to_sharp']
         points = points.unsqueeze(1) if points.dim() == 3 else points
         preds = self.forward(points)
         loss = hydra.utils.instantiate(self.hparams.meta_arch.loss, preds, distances)
@@ -59,7 +53,7 @@ class DepthRegressor(LightningModule):
         return result
 
     def _shared_eval_step(self, batch, batch_idx, prefix):
-        points, distances = batch['image'], batch['distances']
+        points, distances = batch['image'], batch['distance_to_sharp']
         points = points.unsqueeze(1) if points.dim() == 3 else points
         preds = self.forward(points)
 
@@ -101,7 +95,7 @@ class DepthRegressor(LightningModule):
     def _get_dataset(self, partition):
         if hasattr(self, f'{partition}_set') and getattr(self, f'{partition}_set') is not None:
             return getattr(self, f'{partition}_set')
-<<<<<<< HEAD
+
         transform = CompositeTransform([hydra.utils.instantiate(tf) for tf in self.cfg.transforms[partition]])
         return DepthDataset(
             data_dir=self.data_dir,
@@ -109,14 +103,6 @@ class DepthRegressor(LightningModule):
             data_label=self.cfg.data.data_label,
             target_label=self.cfg.data.target_label,
             task=self.task,
-=======
-        transform = CompositeTransform([hydra.utils.instantiate(tf) for tf in self.hparams.transforms[partition]])
-        return LotsOfHdf5Files(
-            data_dir=self.data_dir,
-            io=DepthMapIO,
-            data_label=self.hparams.data.data_label,
-            target_label=self.hparams.data.target_label,
->>>>>>> pl_hydra
             partition=partition,
             transform=transform,
             max_loaded_files=self.hparams.data.max_loaded_files

sharpf/modeling/meta_arch/depth_segmentator.py
@@ -7,15 +7,15 @@ import torch.nn.functional as F
 from pytorch_lightning import TrainResult
 from pytorch_lightning.core.lightning import LightningModule
 from pytorch_lightning.metrics import tensor_metric
+from pytorch_lightning.metrics.sklearns import BalancedAccuracy
 from torch.utils.data import DataLoader
 
 from sharpf.utils.comm import get_batch_size
-from sharpf.utils.losses import balanced_accuracy
+from pytorch_lightning.metrics.functional import stat_scores
 from ..model.build import build_model
 from ...data import DepthMapIO
 from ...utils.abc_utils.hdf5 import DepthDataset
 from ...utils.abc_utils.torch import CompositeTransform
-from ...utils.config import flatten_omegaconf
 
 log = logging.getLogger(__name__)
 
@@ -29,17 +29,16 @@ class DepthSegmentator(LightningModule):
 
     def __init__(self, cfg):
         super().__init__()
-        self.hparams = flatten_omegaconf(cfg)  # there should be better official way later
-        self.cfg = cfg
+        self.hparams = cfg
         self.task = 'segmentation'
-        self.model = build_model(cfg.model)
+        self.model = build_model(self.hparams.model)
         self.example_input_array = torch.rand(1, 1, 64, 64)
-        self.data_dir = hydra.utils.to_absolute_path(self.cfg.data.data_dir)
+        self.data_dir = hydra.utils.to_absolute_path(self.hparams.data.data_dir)
 
-        dist_backend = self.cfg.trainer.distributed_backend
+        dist_backend = self.hparams.trainer.distributed_backend
         if (dist_backend is not None and 'ddp' in dist_backend) or (
-                dist_backend is None and self.cfg.trainer.gpus is not None and (
-                self.cfg.trainer.gpus > 1 or self.cfg.trainer.num_nodes > 1)):
+                dist_backend is None and self.hparams.trainer.gpus is not None and (
+                self.hparams.trainer.gpus > 1 or self.hparams.trainer.num_nodes > 1)):
             log.info('Converting BatchNorm to SyncBatchNorm. Do not forget other batch-dimension dependent operations.')
             self.model = nn.SyncBatchNorm.convert_sync_batchnorm(self.model)
 
@@ -47,36 +46,45 @@ class DepthSegmentator(LightningModule):
         return self.model(x)
 
     def training_step(self, batch, batch_idx):
-        points, distances = batch['image'], batch['distances']
+        points, target = batch['image'], batch['close_to_sharp_mask']
         points = points.unsqueeze(1) if points.dim() == 3 else points
         preds = self.forward(points)
-        loss = hydra.utils.instantiate(self.cfg.meta_arch.loss, preds, distances)
+        loss = hydra.utils.instantiate(self.hparams.meta_arch.loss, preds, target)
         result = TrainResult(minimize=loss)
         result.log('train_loss', loss, prog_bar=True)
         return result
 
     def _shared_eval_step(self, batch, batch_idx, prefix):
-        metric_name = 'balanced_accuracy'
-        metric = balanced_accuracy
-        points, distances = batch['image'], batch['distances']
+        stat_names = ['tpr', 'tnr']
+        points, target = batch['image'], batch['close_to_sharp_mask']
         points = points.unsqueeze(1) if points.dim() == 3 else points
         preds = self.forward(points)
 
-        metric_value = metric(preds, distances)  # (batch)
+        statistics = torch.Tensor(list(map(lambda a: stat_scores(a[0], a[1], class_index=1),
+                                           [preds, target]))).T.to(preds.device) # return: tp, fp, tn, fn, sup
+                                                                # dim: (5, batch)
+
+        tpr = statistics[0] / (statistics[0] + statistics[-2])  # 1(pred=1| true=1) / (1(pred=1| true=1) + 1(pred=0|true=1))
+        tnr = statistics[2] / (statistics[2] + statistics[1])  # 1(pred=0| true=0) / (1(pred=0| true=0) + 1(pred=1|true=0))
+
         # loss = hydra.utils.instantiate(self.cfg.meta_arch.loss, preds, distances)
         # self.logger[0].experiment.add_scalars('losses', {f'{prefix}_loss': loss})
         # TODO Consider pl.EvalResult, once there are good examples how to use it
-        return {f'{metric_name}_sum': metric_value.sum(),
+        return {f'{stat_names[0]}': tpr,
+                f'{stat_names[1]}': tnr,
                 'batch_size': torch.tensor(points.size(0), device=self.device)}
 
     def _shared_eval_epoch_end(self, outputs, prefix):
-        metric_name = 'balanced_accuracy'
+        stat_names = ['tpr', 'tnr']
         metric_sum = 0
         size = 0
         for output in outputs:
-            metric_sum += output[f'{metric_name}_sum']
+            metric_sum += sum((output[f'{stat_names[0]}'] + output[f'{stat_names[1]}']).double() / 2)
             size += output['batch_size']
+
+        metric_name = 'balanced_accuracy'
         mean_metric = gather_sum(metric_sum) / gather_sum(size)
+
         logs = {f'{prefix}_mean_{metric_name}': mean_metric}
         return {f'{prefix}_mean_{metric_name}': mean_metric, 'log': logs}
 
@@ -93,35 +101,35 @@ class DepthSegmentator(LightningModule):
         return self._shared_eval_epoch_end(outputs, prefix='test')
 
     def configure_optimizers(self):
-        optimizer = hydra.utils.instantiate(self.cfg.opt, params=self.parameters())
-        scheduler = hydra.utils.instantiate(self.cfg.scheduler, optimizer=optimizer)
+        optimizer = hydra.utils.instantiate(self.hparams.opt, params=self.parameters())
+        scheduler = hydra.utils.instantiate(self.hparams.scheduler, optimizer=optimizer)
         return [optimizer], [scheduler]
 
     def _get_dataset(self, partition):
         if hasattr(self, f'{partition}_set') and getattr(self, f'{partition}_set') is not None:
             return getattr(self, f'{partition}_set')
-        transform = CompositeTransform([hydra.utils.instantiate(tf) for tf in self.cfg.transforms[partition]])
-        if 'normalisation' in self.cfg.transforms.keys:
-            normalisation = self.cfg.transforms['normalisation']
+        transform = CompositeTransform([hydra.utils.instantiate(tf) for tf in self.hparams.transforms[partition]])
+        if 'normalisation' in self.hparams.transforms.keys():
+            normalisation = self.hparams.transforms['normalisation']
         else:
             normalisation = None
 
         return DepthDataset(
             data_dir=self.data_dir,
             io=DepthMapIO,
-            data_label=self.cfg.data.data_label,
-            target_label=self.cfg.data.target_label,
+            data_label=self.hparams.data.data_label,
+            target_label=self.hparams.data.target_label,
             task=self.task,
             partition=partition,
             transform=transform,
-            max_loaded_files=self.cfg.data.max_loaded_files,
+            max_loaded_files=self.hparams.data.max_loaded_files,
             normalisation=normalisation
         )
 
     def _get_dataloader(self, partition):
         dataset = self._get_dataset(partition)
-        num_workers = self.cfg.data_loader[partition].num_workers
-        batch_size = get_batch_size(self.cfg.data_loader[partition].total_batch_size)
+        num_workers = self.hparams.data_loader[partition].num_workers
+        batch_size = get_batch_size(self.hparams.data_loader[partition].total_batch_size)
         return DataLoader(dataset, batch_size=batch_size, num_workers=num_workers, pin_memory=True)
 
     def setup(self, stage: str):