0229256fbdf57417345d21fe7c39e84e438d7d2b
rakhimovv <risonyo@gmail.com>
2020-09-01 18:51:33 +0300

    fix evalutor docs

--- sharpf/evaluation/evaluator.py
+++ sharpf/evaluation/evaluator.py
@@ -2,9 +2,6 @@ class DatasetEvaluator:
     """
     Base class for a dataset evaluator.
 
-    The function :func:`inference_on_dataset` runs the model over
-    all samples in the dataset, and have a DatasetEvaluator to process the inputs/outputs.
-
     This class will accumulate information of the inputs/outputs (by :meth:`process`),
     and produce evaluation results in the end (by :meth:`evaluate`).
     """
