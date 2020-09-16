a1568dfde478c315cfc19ecd7d724cd22087eb38
rakhimovv <risonyo@gmail.com>
2020-09-17 02:35:34 +0300

    fix visualization

--- sharpf/evaluation/points_visualizaition.py
+++ sharpf/evaluation/points_visualizaition.py
@@ -1,92 +1,31 @@
 import logging
 import os
 
+import k3d
+import matplotlib as mpl
+import matplotlib.cm as cm
 import numpy as np
 import torch
 import torch.nn.functional as F
 
-log = logging.getLogger(__name__)
-
-import k3d
-import matplotlib as mpl
-import matplotlib.cm as cm
-
 from .evaluator import DatasetEvaluator
 from ..utils.comm import all_gather, synchronize, is_main_process
 
-__all__ = ['IllustratorPoints']
-
-
-def get_colors(pred, cmap):
-    hex_colors = []
-    norm = mpl.colors.Normalize(vmin=0, vmax=1)
-    cmap = cmap
-    m = cm.ScalarMappable(norm=norm, cmap=cmap)
-    if len(pred.shape) > 1:
-        pred_color = pred.reshape(-1)
-    else:
-        pred_color = pred
-    for i in pred_color:
-        hex_colors.append(int(mpl.colors.to_hex(m.to_rgba(i)).replace('#', '0x'), 16))
-    hex_colors = np.array(hex_colors, 'uint32')
-    return hex_colors
+log = logging.getLogger(__name__)
 
 
 class IllustratorPoints(DatasetEvaluator):
 
-    def __init__(self, golden_set_ids, k=5, *args, **kwargs):
+    def __init__(self, golden_set_item_ids, k, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.golden_set_ids = golden_set_ids
+        self.golden_set_item_ids = golden_set_item_ids
         self.k = k
         self.reset()
 
     def reset(self):
-        self.metric = []
-        self.relative_golden_ids = []
-
-    def _illustrate_3d(self, data, target, pred, metric):
-
-        plot = k3d.plot(grid_visible=False, axes_helper=0)
-
-        if type(pred) is np.ndarray:
-            col_pred = get_colors(pred, cm.coolwarm_r)
-        else:
-            col_pred = get_colors(pred.cpu().numpy(), cm.coolwarm_r)
-        if type(target) is np.ndarray:
-            col_true = get_colors(target, cm.coolwarm_r)
-        else:
-            col_true = get_colors(target.cpu().numpy(), cm.coolwarm_r)
-        if type(metric) is np.ndarray:
-            col_err = get_colors(metric, cm.jet)
-        else:
-            col_err = get_colors(metric.cpu().numpy(), cm.jet)
-
-        if type(data) is np.ndarray:
-            data_numpy = data
-        else:
-            data_numpy = data.cpu().numpy()
-        points_true = k3d.points(data_numpy, col_true, point_size=0.02, shader='mesh', name='ground truth')
-        points_pred = k3d.points(data_numpy, col_pred, point_size=0.02, shader='mesh', name='prediction')
-        points_err = k3d.points(data_numpy, col_err, point_size=0.02, shader='mesh', name='metric values')
-        colorbar1 = k3d.line([[0, 0, 0], [0, 0, 0]], shader="mesh",
-                             color_range=[0, 1], color_map=k3d.colormaps.matplotlib_color_maps.Jet)
-        colorbar2 = k3d.line([[0, 0, 0], [0, 0, 0]], shader="mesh",
-                             color_range=[0, 1], color_map=k3d.colormaps.matplotlib_color_maps.coolwarm_r)
-
-        plot += points_true + points_pred + points_err + colorbar1 + colorbar2
-
-        return plot
-
-    def illustrate_to_file(self, data, target, pred, metric, name=None):
-        if name is None:
-            name = 'illustrate-points'
-        dr = os.getcwd()
-        plot_3d = self._illustrate_3d(data, target, pred, metric)
-
-        if not os.path.exists(f'{dr}/visuals'):
-            os.mkdir(f'{dr}/visuals')
-        with open(f'{dr}/visuals/{name}.html', 'w') as f:
-            f.write(plot_3d.get_snapshot())
+        self.rmses = []
+        self.indexes = []
+        self.device = None
 
     def process(self, inputs, outputs):
         """
@@ -97,57 +36,108 @@ class IllustratorPoints(DatasetEvaluator):
         target = inputs['distances']
         preds = outputs['pred_distances']
         item_ids = inputs['item_id']
-        dataset_ind = inputs['index']
+        dataset_indexes = inputs['index']
+        self.device = preds.device
 
-        mean_squared_metrics = F.mse_loss(preds, target, reduction='none').reshape(target.shape)
-        root_mean_squared_metrics = torch.sqrt(mean_squared_metrics)
+        batch_size = target.size(0)
+        mean_squared_errors = F.mse_loss(preds, target, reduction='none').view(batch_size, -1).mean(dim=1)  # (batch)
+        root_mean_squared_errors = torch.sqrt(mean_squared_errors)
+        self.rmses.append(root_mean_squared_errors.detach().cpu())
+        self.indexes.append(dataset_indexes.detach().cpu())
 
-        self.metric.append(root_mean_squared_metrics)  # rmse for points
-        tmp = [int(dataset_ind[i]) for i, elem in enumerate(item_ids) if str(elem) in self.golden_set_ids]
-        if len(tmp) != 0:
-            self.relative_golden_ids.append(torch.LongTensor(tmp))
+        # plot golden set
+        for i, item_id in enumerate(item_ids):
+            if str(item_id) in self.golden_set_item_ids:
+                dataset_idx = dataset_indexes[i]
+
+                # dirty hack; slicing because otherwise b'' gets into filename
+                str_item_id = str(item_id)[2:-1]
+
+                filename = f'datasetname={self.dataset_name}_golden_datasetidx={dataset_idx}_itemid={str_item_id}.html'
+
+                # xyz correspond to first 3 channels
+                self._illustrate_to_file(inputs['points'][dataset_idx][:, :3].detach().cpu(),
+                                         target[dataset_idx].detach().cpu(),
+                                         preds[dataset_idx].detach().cpu(),
+                                         F.l1_loss(preds[dataset_idx], target[dataset_idx],
+                                                   reduction='none').detach().cpu(),
+                                         name=filename)
 
     def evaluate(self):
+
+        self.rmses = torch.cat(self.rmses)
+        self.indexes = torch.cat(self.indexes)
+        assert self.rmses.size() == self.indexes.size()
+
         # gather results across gpus
-        log.info('evaluate visualization')
         synchronize()
-        metrics = []
-        for elem in all_gather(self.metric):
-            for e in elem:
-                metrics.append(e)
-        metrics = torch.cat(metrics)
-
-        if len(self.relative_golden_ids) == 0:
-            return {'scalars': {}, 'images': {}}
-        relative_golden_ids = []
-        for tmp in all_gather(self.relative_golden_ids):
-            relative_golden_ids.append(torch.LongTensor(torch.cat(tmp)))
-        relative_golden_ids = torch.LongTensor(torch.cat(relative_golden_ids))
-
-        metrics_golden = metrics[relative_golden_ids].cpu().numpy()
-        # calculate quantiles
-        k_best_idx = np.argsort(torch.sqrt(metrics.mean(dim=1)).cpu().numpy(), axis=0)[::-1][:self.k]
-        k_worst_idx = np.argsort(torch.sqrt(metrics.mean(dim=1)).cpu().numpy(), axis=0)[:self.k]
-
-        log.info('get input data')
+        rmses = torch.cat(all_gather(self.rmses))
+        indexes = torch.cat(all_gather(self.indexes))
 
         if is_main_process():
-            log.info('saving visualization')
-            for i in relative_golden_ids:
-                item_golden = self.dataset[i]
-                pred_golden = self.model(item_golden['points'].unsqueeze(0).to('cuda'))  # predict with model
-                self.illustrate_to_file(item_golden['points'], item_golden['distances'], pred_golden, metrics_golden[i],
-                                        name=f'illustration-points_golden-set_{i}')
 
-            for i in range(len(k_best_idx)):
-                item_best = self.dataset[k_best_idx[i]]
-                item_worst = self.dataset[k_worst_idx[i]]
-                pred_best = self.model(item_best['points'].unsqueeze(0).to('cuda'))
-                self.illustrate_to_file(item_best['points'], item_best['distances'], pred_best, metrics[k_best_idx[i]],
-                                        name=f'illustration-points_best-metric_{i}')
-                pred_worst = self.model(item_worst['points'].unsqueeze(0).to('cuda'))
-                self.illustrate_to_file(item_worst['points'], item_worst['distances'], pred_worst,
-                                        metrics[k_worst_idx[i]],
-                                        name=f'illustration-points_worst-metric_{i}')
-        log.info('!saved visualization')
+            def plot_top_k(type: str):
+                assert type in ['best', 'worst']
+
+                rmse_top_k_idxs = indexes[torch.topk(rmses, self.k, largest=type == 'worst').indices]
+                for k_i, index in enumerate(rmse_top_k_idxs):
+                    item = self.dataset[index]
+                    pred = self.model(item['points'].unsqueeze(0).to(self.device)).detach().cpu().squeeze(0)
+                    l1_errors = F.l1_loss(pred, item['distances'], reduction='none')
+                    str_item_id = str(item['item_id'])[
+                                  2:-1]  # dirty hack; slicing because otherwise b'' gets into filename
+                    filename = f'datasetname={self.dataset_name}_{type}{k_i + 1}_datasetidx={index}_itemid={str_item_id}.html'
+
+                    # xyz correspond to first 3 channels
+                    self._illustrate_to_file(item['points'][:, :3], item['distances'], pred, l1_errors, name=filename)
+
+            plot_top_k('best')
+            plot_top_k('worst')
+
         return {'scalars': {}, 'images': {}}
+
+    def _get_colors(self, pred, cmap):
+        hex_colors = []
+        norm = mpl.colors.Normalize(vmin=0, vmax=1)
+        m = cm.ScalarMappable(norm=norm, cmap=cmap)
+        if len(pred.shape) > 1:
+            pred_color = pred.reshape(-1)
+        else:
+            pred_color = pred
+        for i in pred_color:
+            hex_colors.append(int(mpl.colors.to_hex(m.to_rgba(i)).replace('#', '0x'), 16))
+        hex_colors = np.array(hex_colors, 'uint32')
+        return hex_colors
+
+    def _illustrate_3d(self, xyz: torch.Tensor, target: torch.Tensor, pred: torch.Tensor, errors: torch.Tensor):
+
+        plot = k3d.plot(grid_visible=False, axes_helper=0)
+
+        col_pred = self._get_colors(pred, cm.coolwarm_r)
+        col_true = self._get_colors(target, cm.coolwarm_r)
+        col_err = self._get_colors(errors, cm.jet)
+
+        points_true = k3d.points(xyz, col_true, point_size=0.02, shader='mesh', name='ground truth')
+        points_pred = k3d.points(xyz, col_pred, point_size=0.02, shader='mesh', name='prediction')
+        points_err = k3d.points(xyz, col_err, point_size=0.02, shader='mesh', name='metric values')
+        colorbar1 = k3d.line([[0, 0, 0], [0, 0, 0]], shader="mesh",
+                             color_range=[0, 1], color_map=k3d.colormaps.matplotlib_color_maps.Jet)
+        colorbar2 = k3d.line([[0, 0, 0], [0, 0, 0]], shader="mesh",
+                             color_range=[0, 1], color_map=k3d.colormaps.matplotlib_color_maps.coolwarm_r)
+
+        plot += points_true + points_pred + points_err + colorbar1 + colorbar2
+
+        return plot
+
+    def _illustrate_to_file(self, xyz: torch.Tensor, target: torch.Tensor, pred: torch.Tensor, errors: torch.Tensor,
+                            name: str):
+        assert name is not None
+
+        plot_3d = self._illustrate_3d(xyz, target, pred, errors)
+
+        current_dir = os.getcwd()
+        visuals_dir = os.path.join(current_dir, 'visuals')
+        if not os.path.exists(visuals_dir):
+            os.mkdir(visuals_dir)
+        with open(os.path.join(visuals_dir, name), 'w') as f:
+            f.write(plot_3d.get_snapshot())
