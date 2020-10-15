3214eff8f9d690285eba3d35e52d9b761f473560
rakhimovv <risonyo@gmail.com>
2020-10-16 00:40:07 +0300

    add filter expressions for predictions saving

--- sharpf/evaluation/predictions_saving.py
+++ sharpf/evaluation/predictions_saving.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import numpy as np
+import torch
 
 from .evaluator import DatasetEvaluator
 
@@ -9,6 +10,10 @@ log = logging.getLogger(__name__)
 
 
 class PredictionsSaving(DatasetEvaluator):
+
+    def __init__(self, filter_expressions=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filter_expressions = filter_expressions if filter_expressions is not None else []
 
     def reset(self):
         pass
@@ -19,11 +24,30 @@ class PredictionsSaving(DatasetEvaluator):
             inputs (dict): the inputs to a model.
             outputs (dict): the outputs of a model.
         """
-        for i, index in enumerate(inputs['index']):
-            self.save_tensor(outputs['distances'][i], f"{self.dataset_name}_{index.item()}.npy")
+        # apply filters
+        final_mask = torch.ones_like(inputs['index'], device=inputs['index'].device, dtype=torch.bool)
+        for key, op, value in self.filter_expressions:
+            if op == '=':
+                mask = inputs[key] == value
+            elif op == '>':
+                mask = inputs[key] > value
+            elif op == '>=':
+                mask = inputs[key] >= value
+            else:
+                raise ValueError
+            final_mask = final_mask * mask
+
+        if final_mask.sum() == 0:
+            return
+
+        inds = final_mask.nonzero(as_tuple=True)[0]
+        dataset_indexes = torch.index_select(inputs['index'], 0, inds)
+        preds = torch.index_select(outputs['distances'], 0, inds)
+
+        for i, index in enumerate(dataset_indexes):
+            self.save_tensor(preds[i], f"{self.dataset_name}_{index.item()}.npy")
 
     def save_tensor(self, tensor, name):
-        print(tensor.shape, name)
         current_dir = os.getcwd()
         save_dir = os.path.join(current_dir, 'predictions')
         if not os.path.exists(save_dir):
