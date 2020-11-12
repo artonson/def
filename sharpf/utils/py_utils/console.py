510e331b41996c9fa20174cf4b9e0e80451eca3f
Alexey Artemov <a.artemov@skoltech.ru>
2020-11-12 12:26:14 +0300

    flush in sharpf/utils/py_utils/console.py

--- sharpf/utils/py_utils/console.py
+++ sharpf/utils/py_utils/console.py
@@ -10,4 +10,5 @@ def eprint(*args, **kwargs):
 def eprint_t(*args, **kwargs):
     ts_fmt = datetime.now().strftime("%d.%m.%Y %H:%M:%S.%f ")
     print(ts_fmt, multiprocessing.current_process().name, *args, file=sys.stderr, **kwargs)
+    sys.stderr.flush()
 
