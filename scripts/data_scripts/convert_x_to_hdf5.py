c984110ba9e715121822a7caac81a1b23e5d96b4
Alexey Artemov <a.artemov@skoltech.ru>
2021-02-23 23:45:24 +0300

    w.i.p. on convert_x_to_hdf5.py

--- scripts/data_scripts/convert_x_to_hdf5.py
+++ scripts/data_scripts/convert_x_to_hdf5.py
@@ -2,11 +2,14 @@
 
 import argparse
 
+import numpy as np
 import pyparsing as pp
 
 # This script is based on the DirectX format presented in
 # http://paulbourke.net/dataformats/directx/
 # (but may not 100% fully implement everything).
+import sharpf.utils.camera_utils.rangevision_utils as rv_utils
+
 
 def create_parser():
     # parse header
@@ -21,13 +24,13 @@ def create_parser():
 
     # parse data objects
     opener = (
-                     pp.Literal('Frame') ^
-                     pp.Literal('Processing') ^
-                     pp.Literal('RV_Calibration') ^
-                     pp.Literal('FrameTransformMatrix') ^
-                     pp.Literal('Mesh')) + \
-             pp.Word(pp.alphas + pp.nums + '_')[0,2] + \
-             pp.Literal('{')
+            pp.Literal('Frame') ^
+            pp.Literal('Processing') ^
+            pp.Literal('RV_Calibration') ^
+            pp.Literal('FrameTransformMatrix') ^
+            pp.Literal('Mesh')) + \
+        pp.Word(pp.alphas + pp.nums + '_')[0,2] + \
+        pp.Literal('{')
     closer = pp.Literal('}')
     content = pp.Word(pp.nums + ',.;-')
     data_objects = pp.nestedExpr(
@@ -40,18 +43,43 @@ def create_parser():
     return directx_parser
 
 
+def get_floats(s):
+    s = s.strip(';,')
+    return [float(value) for value in s.split(',')]
+
+
 def transform_parsed_result(result):
-    frame_obj = result[-1]
-    processing, frame_group = frame_obj
+    header, frame_side = result[:-1], result[-1]
+    frame_group = frame_side[1]
 
     scans = []
     for frame in frame_group[1:]:
         calibration, transform, mesh = frame
 
+        rotation_matrix, \
+            focal_length, \
+            angles, \
+            translation, \
+            pixel_xy, \
+            center_xy, \
+            correction = [get_floats(part) for part in calibration]
+
+        extrinsics = rv_utils.get_camera_extrinsic(angles, translation)
+        intrinsics_f = rv_utils.get_camera_intrinsic_f(focal_length[0])
+        intrinsics_s = rv_utils.get_camera_intrinsic_s(
+            pixel_xy[0] * 1e3,
+            pixel_xy[1] * 1e3,
+            center_xy[0],
+            center_xy[1])
+        intrinsics = np.dstack((intrinsics_f, intrinsics_s))
+
+        alignment_transform = get_floats(transform[0])
+        alignment_transform = np.array(alignment_transform).reshape((4, 4)).T
+
         scans.append({
             'image': image,
             'extrinsics': extrinsics,
-            'intrinsics': intrinsics_f
+            'intrinsics': intrinsics,
         })
 
 
