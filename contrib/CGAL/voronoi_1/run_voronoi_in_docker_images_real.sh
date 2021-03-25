a1e3ffb5c84ec360c2caab25e62ee24576038bec
albmatveev <31800984+albmatveev@users.noreply.github.com>
2021-03-25 22:58:56 +0300

    Add Voronoi test scripts

---
+++ contrib/CGAL/voronoi_1/run_voronoi_in_docker_images_real.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+#SBATCH --job-name=voronoi
+#SBATCH --output=array_%A_%a.out
+#SBATCH --error=array_%A_%a.err
+#SBATCH --time=24:00:00
+#SBATCH --partition=htc
+#SBATCH --cpus-per-task=24
+#SBATCH --ntasks-per-node=1
+
+# set -e
+# set -x
+
+# example launch string:
+# ./run_voronoi_in_docker.sh -R 0.2 -r 0.05 -t 0.15 -i /home/artonson/data -o /home/artonson/output -v
+#   R, r, t:                    Voronoi method parameters
+#   /home/artonson/data:        the data directory where the training sample resides
+#   /home/artonson/output:      the directory where the output files are to be written
+#   v:                          be verbose
+
+usage() { echo "Usage: ${0} -i input_file -o output_file [-R offset_radius] [-r conv_radius] [-t threshold] [-j num_jobs]" >&2; }
+
+V_OFFSET_RADIUS=0.2
+V_CONV_RADIUS=0.1
+V_THRESHOLD=0.16
+NUM_JOBS=1
+while getopts "i:o:R:r:t:j:v:" opt
+do
+    case ${opt} in
+        i) INPUT_HDF5_FILENAME=${OPTARG} ;;
+        o) OUTPUT_HDF5_FILENAME=${OPTARG} ;;
+        R) V_OFFSET_RADIUS=${OPTARG} ;;
+        r) V_CONV_RADIUS=${OPTARG} ;;
+        t) V_THRESHOLD=${OPTARG} ;;
+        j) NUM_JOBS=${OPTARG} ;;
+        v) VARLEN=${OPTARG} ;;
+        *) usage; exit 1 ;;
+    esac
+done
+
+# HOST_<anything> refers to paths OUTSIDE container, i.e. on host machine
+# CONT_<anything> refers to paths INSIDE container
+
+[[ -f ${INPUT_HDF5_FILENAME} ]] || { echo "input_file not set or empty"; usage; exit 1; }
+HOST_INPUT_DIR="$( cd "$( dirname "${INPUT_HDF5_FILENAME}" )" >/dev/null 2>&1 && pwd )"
+INPUT_FILENAME="$( basename "${INPUT_HDF5_FILENAME}" )"
+CONT_INPUT_DIR="/input"
+
+HOST_OUTPUT_DIR="$( cd "$( dirname "${OUTPUT_HDF5_FILENAME}" )" >/dev/null 2>&1 && pwd )"
+[[ -d ${HOST_OUTPUT_DIR} ]] || { echo "output directory ${HOST_OUTPUT_DIR} needs to be created first"; usage; exit 1; }
+OUTPUT_FILENAME="$( basename "${OUTPUT_HDF5_FILENAME}" )"
+CONT_OUTPUT_DIR="/output"
+
+HOST_CODE_DIR="/trinity/home/a.matveev/sharp_features/contrib/CGAL/voronoi_1"
+CONT_CODE_DIR="/code"
+
+HOST_PY_DIR="/trinity/home/a.matveev/sharp_features/contrib/hdf5_utils"
+CONT_PY_DIR="/code/hdf5_utils"
+
+
+SIMAGE_FILENAME=/gpfs/gpfs0/a.matveev/root_cgal_4-14-2020-08-27-8fd1f854b75e.sif
+# docker inspect --type=image ${IMAGE_NAME} >/dev/null || docker pull ${IMAGE_NAME}
+
+CONTAINER_NAME="3ddl.$(whoami).$(uuidgen).voronoi_R${V_OFFSET_RADIUS}_r${V_CONV_RADIUS}_thresh${V_THRESHOLD}.sharp_features"
+
+echo "******* LAUNCHING IMAGE ${IMAGE_NAME} IN CONTAINER ${CONTAINER_NAME} *******"
+echo "  "
+echo "  HOST OPTIONS:"
+echo "  input path:           ${HOST_INPUT_DIR}"
+echo "  output path:          ${HOST_OUTPUT_DIR}"
+echo "  code path:            ${HOST_CODE_DIR}"
+echo "  py code path:         ${HOST_PY_DIR}"
+echo "  "
+echo "  CONTAINER OPTIONS:"
+echo "  input path:           ${CONT_INPUT_DIR}"
+echo "  output path:          ${CONT_OUTPUT_DIR}"
+echo "  code path:            ${CONT_CODE_DIR}"
+echo "  py code path:         ${CONT_PY_DIR}"
+echo "  logs path:            ${CONT_OUTPUT_DIR}"
+
+
+singularity exec \
+    --bind ${HOST_INPUT_DIR}:${CONT_INPUT_DIR} \
+    --bind ${HOST_OUTPUT_DIR}:${CONT_OUTPUT_DIR} \
+    --bind ${HOST_CODE_DIR}:${CONT_CODE_DIR} \
+    --bind ${HOST_PY_DIR}:${CONT_PY_DIR} \
+    --bind "${PWD}":/run/user \
+  "${SIMAGE_FILENAME}" \
+    /bin/bash \
+        -c "${CONT_CODE_DIR}/run_voronoi_real_images.sh \\
+            -i ${CONT_INPUT_DIR}/${INPUT_FILENAME} \\
+            -o ${CONT_OUTPUT_DIR}/${OUTPUT_FILENAME} \\
+            -R ${V_OFFSET_RADIUS} \\
+            -r ${V_CONV_RADIUS} \\
+            -t ${V_THRESHOLD} \\
+            -j ${NUM_JOBS} \\
+            -v ${VARLEN}
+        1>${CONT_OUTPUT_DIR}/out.out \\
+        2>${CONT_OUTPUT_DIR}/err.err"
+
---
+++ contrib/CGAL/voronoi_1/run_voronoi_points.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+usage() { echo "Usage: ${0} -i input_file -o output_file [-R offset_radius] [-r conv_radius] [-t threshold] [-j num_jobs]" >&2; }
+
+V_OFFSET_RADIUS=0.2
+V_CONV_RADIUS=0.1
+V_THRESHOLD=0.16
+NUM_JOBS=1
+while getopts "i:o:R:r:t:j:v:" opt
+do
+    case ${opt} in
+        i) INPUT_HDF5_FILENAME=${OPTARG} ;;
+        o) OUTPUT_HDF5_FILENAME=${OPTARG} ;;
+        R) V_OFFSET_RADIUS=${OPTARG} ;;
+        r) V_CONV_RADIUS=${OPTARG} ;;
+        t) V_THRESHOLD=${OPTARG} ;;
+        j) NUM_JOBS=${OPTARG} ;;
+        v) VARLEN=${OPTARG} ;;
+        *) usage; exit 1 ;;
+    esac
+done
+
+# check input variables
+[[ -f ${INPUT_HDF5_FILENAME} ]] || { echo "input_file not set or empty"; usage; exit 1; }
+OUTPUT_DIR="$( cd "$( dirname "${OUTPUT_HDF5_FILENAME}" )" >/dev/null 2>&1 && pwd )"
+[[ -d ${OUTPUT_DIR} ]] || { echo "output directory ${HOST_OUTPUT_DIR} needs to be created first"; usage; exit 1; }
+
+# prepate directories and needed environment
+INPUT_XYZ_DIR=$( mktemp -d )
+OUTPUT_XYZ_DIR=$( mktemp -d )
+
+PY_SRC_DIR=/code/hdf5_utils
+SPLIT_SCRIPT=${PY_SRC_DIR}/split_hdf5_points.py
+MERGE_SCRIPT=${PY_SRC_DIR}/merge_hdf5.py
+BIN_DIR=/home/user/code/voronoi_1
+BINARY=${BIN_DIR}/voronoi_1
+
+DATA_LABEL="points"
+TARGET_LABEL="distances"
+
+# Run commands
+python3 ${SPLIT_SCRIPT} \
+    --label ${DATA_LABEL} \
+    --output_dir ${INPUT_XYZ_DIR} \
+    ${INPUT_HDF5_FILENAME}
+
+find ${INPUT_XYZ_DIR} -type f -name "*.xyz" \
+    | parallel \
+        --jobs ${NUM_JOBS} \
+            ${BINARY} \
+            -f {} \
+            -R ${V_OFFSET_RADIUS} \
+            -r ${V_CONV_RADIUS} \
+            -t ${V_THRESHOLD} \
+            -o ${OUTPUT_XYZ_DIR} \
+        {}
+
+python3 ${MERGE_SCRIPT} \
+    -i ${OUTPUT_XYZ_DIR} \
+    -o ${OUTPUT_HDF5_FILENAME} \
+    --label ${TARGET_LABEL} \
+    --input_format txt \
+    --varlen ${VARLEN}
+
+rm -rf ${INPUT_XYZ_DIR} ${OUTPUT_XYZ_DIR}
