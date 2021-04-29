08df1fcf29b9a63e22f51967f5f7dd453f200682
Alexey Artemov <a.artemov@skoltech.ru>
2021-04-29 18:27:56 +0300

    update plot_views

--- sharpf/utils/plotting.py
+++ sharpf/utils/plotting.py
@@ -9,133 +9,126 @@ from IPython.display import Image
 import randomcolor
 
 import sharpf.utils.abc_utils.abc.feature_utils as def_feature_utils
-
-
-def display_sharpness(mesh=None, mesh_color=0xbbbbbb,
-                      plot_meshvert=True, meshvert_color=0x666666, meshvert_psize=0.0025,
-                      samples=None, samples_distances=None, samples_color=0x0000ff, samples_psize=0.002,
-                      sharp_vert=None, sharpvert_color=0xff0000, sharpvert_psize=0.0025,
-                      sharp_curves=None, sharpcurve_color=None, sharpcurve_width=0.0025,
-                      directions=None, directions_width=0.0025,
-                      as_image=False, plot_height=768,
-                      point_shader='3d'):
-    """A method for plotting the sharp curves on top of the mesh.
-    Use it to display meshes, point clouds, distance or direction fields
-    along with ground-truth curve annotations.
-
-    :param mesh: the mesh to display (ideally trimesh.Trimesh but namedtuple
-                with `vertices` and `faces` attributes should work, too).
-                Set to None to turn mesh display off.
-    :type mesh: trimesh.Trimesh
-
-    :param mesh_color: the hex color of the mesh to plot
-    :type mesh_color: int
-
-    :param plot_meshvert: if True, displays mesh vertices as points of given radius/color
-    :type plot_meshvert: bool
-
-    :param meshvert_color: the hex color of the mesh vertices to plot
-    :type meshvert_color: int
-
-    :param meshvert_psize: size of mesh vertices
-    :type meshvert_psize: float
-
-    :param samples:
-    :param samples_color:
-    :param samples_psize:
-    :type samples:
-    :type samples_color:
-    :type samples_psize:
-
-    :param samples_distances:
-
-    :param sharp_vert:
-    :param sharpvert_color:
-    :param sharpvert_psize:
-
-    :param sharp_curves:
-    :param sharpcurve_color:
-    :param sharpcurve_width:
-
-    :param directions:
-    :param directions_width:
-    :param as_image:
-    :param plot_height:
-
-    :return:
-
-    Usage examples
-    ==============
-    >>>
-    with ABCChunk(['/data/abc_0056_obj_v00.7z', '/data/abc_0056_feat_v00.7z']) as data_holder:
-    item = data_holder[100]
-
-    mesh = trimesh_load(item.obj)
-    features = yaml.load(item.feat, Loader=yaml.Loader)
-
-    display_sharpness(mesh=mesh,
-    """
-    plot = k3d.plot(height=plot_height)
-
-    if None is not mesh:
-        plot += k3d.mesh(mesh.vertices, mesh.faces,
-                         color=mesh_color)
-
-        if plot_meshvert:
-            plot += k3d.points(mesh.vertices,
-                               point_size=meshvert_psize, color=meshvert_color, shader=point_shader)
-
-    if None is not samples:
-        colors = None
-        if None is not samples_distances:
-            max_dist = np.max(samples_distances)
-
-            colors = k3d.helpers.map_colors(
-                samples_distances, k3d.colormaps.basic_color_maps.WarmCool, [0, max_dist]
-            ).astype(np.uint32)
-            k3d_points = k3d.points(samples, point_size=samples_psize, colors=colors)
-        else:
-            k3d_points = k3d.points(samples, point_size=samples_psize, color=samples_color)
-        plot += k3d_points
-        k3d_points.shader = '3d'
-
-        if None is not directions:
-            directions_to_plot = np.hstack((samples, samples + directions))
-
-            for i, dir_to_plot in enumerate(directions_to_plot):
-                dir_to_plot = dir_to_plot.reshape((2, 3))
-                if np.all(dir_to_plot[0] == dir_to_plot[1]):
-                    continue
-                color = int(colors[i]) if None is not colors else samples_color
-                plt_line = k3d.line(dir_to_plot,
-                                    shader='mesh', width=directions_width, color=color)
-                plot += plt_line
-
-    if None is not sharp_vert:
-        k3d_points = k3d.points(sharp_vert,
-                                point_size=sharpvert_psize, color=sharpvert_color)
-        plot += k3d_points
-        k3d_points.shader = '3d'
-
-        if None is not sharp_curves:
-            rand_color = randomcolor.RandomColor()
-            for i, vert_ind in sharp_curves.items():
-                sharp_points_curve = mesh.vertices[vert_ind]
-                if None is not sharpcurve_color:
-                    color = sharpcurve_color
-                else:
-                    color = rand_color.generate(hue='red')[0]
-                    color = int('0x' + color[1:], 16)
-                plt_line = k3d.line(sharp_points_curve,
-                                    shader='mesh', width=sharpcurve_width, color=color)
-                plot += plt_line
-
-    plot.grid_visible = False
-    plot.display()
-
-    if as_image:
-        plot.fetch_screenshot()
-        return Image(data=b64decode(plot.screenshot))
+#
+#
+# def display_sharpness(
+#         mesh: trimesh.base.Trimesh = None,
+#         samples: np.ndarray = None,
+#         distances=None,
+#         directions=None,
+#         sharp_vertices=None,
+#         sharp_curves=None,
+#         directions_width=0.0025,
+#         plot_meshvert=True,
+#         samples_color=0x0000ff,
+#         samples_psize=0.002,
+#         mesh_color=0xbbbbbb,
+#         meshvert_color=0x666666,
+#         meshvert_psize=0.0025,
+#         sharpvert_color=0xff0000,
+#         sharpvert_psize=0.0025,
+#         sharpcurve_color=None,
+#         sharpcurve_width=0.0025,
+#         plot_height=768,
+#         plot_bgcolor=0xffffff,
+#         distances_cmap=k3d.colormaps.matplotlib_color_maps.coolwarm_r,
+#         plot=None,
+#         display=True,
+#         max_distance_to_feature=1.0):
+#
+#     if None is plot:
+#         plot = k3d.plot(
+#             grid_visible=True,
+#             height=plot_height,
+#             background_color=plot_bgcolor,
+#             camera_auto_fit=True)
+#
+#     if None is not mesh:
+#         plot += k3d.mesh(
+#             mesh.vertices,
+#             mesh.faces,
+#             color=mesh_color,
+#             side='both',
+#             flat_shading=False)
+#
+#         if plot_meshvert:
+#             k3d_points = k3d.points(mesh.vertices,
+#                                     point_size=meshvert_psize, color=meshvert_color)
+#             plot += k3d_points
+#             k3d_points.shader = 'flat'
+#
+#     if None is not samples:
+#         colors = None
+#         if None is not samples_distances and not np.all(samples_distances == 1.0):
+#             colors = k3d.helpers.map_colors(
+#                 samples_distances, distances_cmap, [0, max_distance_to_feature]
+#             ).astype(np.uint32)
+#             k3d_points = k3d.points(samples, point_size=samples_psize, colors=colors)
+#         else:
+#             v = -np.array([0., 1., 0.])
+#             max_dist = np.max(np.dot(samples, v))
+#             min_dist = np.min(np.dot(samples, v))
+#             colors = k3d.helpers.map_colors(
+#                 np.dot(samples, v), k3d.colormaps.matplotlib_color_maps.viridis, [min_dist, max_dist]
+#             ).astype(np.uint32)
+#             k3d_points = k3d.points(samples, point_size=samples_psize, colors=colors)
+#
+#         plot += k3d_points
+#         k3d_points.shader = 'flat'
+#
+#         if None is not directions:
+#             colors = k3d.helpers.map_colors(
+#                 samples_distances, distances_cmap, [0, max_distance_to_feature]
+#             ).astype(np.uint32)
+#             colors = [(c, c) for c in colors]
+#
+#             vectors = k3d.vectors(
+#                 samples,
+#                 directions * samples_distances[..., np.newaxis],
+#                 use_head=False,
+#                 line_width=directions_width,
+#                 colors=colors)
+#             #             print(vectors)
+#             plot += vectors
+#
+#     #             directions_to_plot = np.hstack((samples, samples + directions))
+#
+#     #             for i, dir_to_plot in enumerate(directions_to_plot):
+#     #                 dir_to_plot = dir_to_plot.reshape((2, 3))
+#     #                 if np.all(dir_to_plot[0] == dir_to_plot[1]):
+#     #                     continue
+#     #                 color = int(colors[i]) if None is not colors else samples_color
+#     #                 plt_line = k3d.line(dir_to_plot,
+#     #                                     shader='mesh', width=directions_width, color=color)
+#     #                 plot += plt_line
+#
+#     if None is not sharp_vert:
+#         k3d_points = k3d.points(sharp_vert,
+#                                 point_size=sharpvert_psize, color=sharpvert_color)
+#         plot += k3d_points
+#         k3d_points.shader = 'flat'
+#
+#         if None is not sharp_curves:
+#             if None is not sharpcurve_color:
+#                 color = sharpcurve_color
+#             else:
+#                 import randomcolor
+#                 rand_color = randomcolor.RandomColor()
+#             for i, vert_ind in enumerate(sharp_curves):
+#                 sharp_points_curve = mesh.vertices[vert_ind]
+#
+#                 if None is sharpcurve_color:
+#                     color = rand_color.generate(hue='red')[0]
+#                     color = int('0x' + color[1:], 16)
+#                 plt_line = k3d.line(sharp_points_curve,
+#                                     shader='mesh', width=sharpcurve_width, color=color)
+#                 plot += plt_line
+#
+#     plot.grid_visible = False
+#     if display:
+#         plot.display()
+#
+#     return plot
 
 
 def display_depth_sharpness(
@@ -284,7 +277,7 @@ def illustrate_camera(
 
 def plot_views(
         views,
-        mesh,
+        mesh=None,
         camera_l=1,
         camera_w=1,
 ):
@@ -302,24 +295,25 @@ def plot_views(
             point_size=0.25,
             color=get_random_color(hue='green'),
             shader='flat')
-        
+
         plot += illustrate_camera(
             view.pose,
             l=camera_l,
             w=camera_w)
 
-    plot += k3d.mesh(
-        mesh.vertices,
-        mesh.faces,
-        color=0xaaaaaa,
-        flat_shading=False)
+    if mesh is not None:
+        plot += k3d.mesh(
+            mesh.vertices,
+            mesh.faces,
+            color=0xaaaaaa,
+            flat_shading=False)
 
     return plot
 
 
 def display_patch_decomposition(
-    mesh,
-    features,
+        mesh,
+        features,
 ):
     plot = k3d.plot(grid_visible=True, height=768)
 
