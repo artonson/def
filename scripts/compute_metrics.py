fix metrics computation

scripts/compute_metrics.py
@@ -22,14 +22,14 @@ def main(options):
         io=fusion_io.FusedPredictionsIO,
         preload=PreloadTypes.LAZY,
         labels=['distances'])
-    true_distances = true_dataset[0]['distances']
+    true_distances = {'distances': true_dataset[0]['distances']}
 
     pred_dataset = Hdf5File(
         options.pred_filename,
         io=fusion_io.FusedPredictionsIO,
         preload=PreloadTypes.LAZY,
         labels=['distances'])
-    pred_distances = pred_dataset[0]['distances']
+    pred_distances = {'distances': pred_dataset[0]['distances']}
 
     rmse = nm.RMSE()
     q95rmse = nm.RMSEQuantile(0.95)
@@ -37,7 +37,7 @@ def main(options):
     bad_points_1r = nm.BadPoints(r1)
     r4 = options.resolution_3d * 4
     bad_points_4r = nm.BadPoints(r4)
-    iou = nm.IOU()
+    iou = nm.IOU(r1)
 
     all_mask = nm.DistanceLessThan(np.max(true_distances) + 1e-6, name='ALL')
     RMSE_ALL = nm.MaskedMetric(all_mask, rmse)

sharpf/metrics/numpy_metrics.py
@@ -46,7 +46,7 @@ class BadPoints(Metric):
         self._normalize = normalize
 
     def __str__(self):
-        return 'Bad {0:3.3g}'.format(np.sqrt(self._threshold_sq))
+        return 'BadPoints({0:3.3g})'.format(np.sqrt(self._threshold_sq))
 
     def __call__(self, true_instance, pred_label):
         diff_sq = PointwiseSquaredError()(true_instance, pred_label)
@@ -66,7 +66,7 @@ class RMSEQuantile(Metric):
         self._normalize = normalize
 
     def __str__(self):
-        return 'Q {0:3.3g}'.format(self._proba)
+        return 'q{0:d}RMSE'.format(int(self._proba * 100))
 
     def __call__(self, true_instance, pred_label):
         diff_sq = PointwiseSquaredError()(true_instance, pred_label)
@@ -127,4 +127,19 @@ class RescaledMetric(Metric):
 
 
 class IOU(Metric):
-    pass
+    """Intersection over union."""
+    def __init__(self, threshold):
+        self._threshold = threshold
+
+    def __str__(self):
+        return 'IOU'
+
+    def __call__(self, true_instance, pred_label):
+        from sklearn.metrics import jaccard_score
+        y_true = (true_instance['distances'] < self._threshold).astype(float)
+        y_pred = (pred_label['distances'] < self._threshold).astype(float)
+        iou = jaccard_score(y_true, y_pred)
+        # intersection = y_pred * y_true
+        # union = y_true + y_pred - intersection
+        # iou = intersection.sum() / union.sum()
+        return iou