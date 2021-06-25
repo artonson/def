01baf9c77e91f185cdf0ed5ba6e9e072fbf3a5b2
emil <emilzq@bk.ru>
2021-06-25 10:41:21 +0000

    rewrite combiner to create symlinks

--- scripts/data_scripts/combine_patches_for_model.py
+++ scripts/data_scripts/combine_patches_for_model.py
@@ -1,38 +1,57 @@
 import os
 import argparse
-import numpy as np
-import h5py
+import pathlib
 
 def main(options):
-    
-    g = h5py.File(options.input_file, "r")
-    n_patches = g['points'].shape[0]
-    print("number of patches ", n_patches)
-
-    if os.path.exists(options.output_file):
-        os.remove(options.output_file)
-    f = h5py.File(options.output_file, "w")
-    dt = h5py.vlen_dtype(np.dtype('float32'))
-    points = f.create_dataset('points', (n_patches,), dtype=dt)
-    distances = f.create_dataset('distances', (n_patches,), dtype=dt)
-    for idx, item in enumerate(g['points']):
-        points[idx] = item
-    for idx, item in enumerate(g['distances']):
-        distances[idx] = item
-    
-    for key in g.keys():
-        if key == "points" or key == "distances":
-            continue
-        else:
-            dset = f.create_dataset(key, data=g[key])
-    print("written ", options.output_file)
+    hdf5_dict = {
+        "low_res_whole.json": {"0.0": []},
+        "med_res_whole.json": {"0.0": []},
+        "high_res_whole.json": {
+            "0.0": [],
+            "0.005": [],
+            "0.02": [],
+            "0.08": [],
+        },
+    }
+    with open(options.hdf5_list) as f:
+        for line in f:
+            line = line.strip()
+            if "low_res_whole.json" in line:
+                hdf5_dict["low_res_whole.json"]["0.0"].append(line)
+            elif "med_res_whole.json" in line:
+                hdf5_dict["med_res_whole.json"]["0.0"].append(line)
+            elif "high_res_whole.json" in line:
+                hdf5_dict["high_res_whole.json"]["0.0"].append(line)
+            elif "high_res_whole_0.005.json" in line:
+                hdf5_dict["high_res_whole.json"]["0.005"].append(line)
+            elif "high_res_whole_0.02.json" in line:
+                hdf5_dict["high_res_whole.json"]["0.02"].append(line)
+            elif "high_res_whole_0.08.json" in line:
+                hdf5_dict["high_res_whole.json"]["0.08"].append(line)
+    output_file = []
+    for resol in hdf5_dict.keys():
+        for noise in hdf5_dict[resol].keys():
+            path = pathlib.Path(options.prefix_path) / resol / noise
+            try:
+                path.mkdir(parents=True, exist_ok=False)
+            except FileExistsError:
+                pass
+            for idx, hdf5_file in enumerate(hdf5_dict[resol][noise]):
+                train_file = path / f'train_{idx}.hdf5'
+                os.symlink(hdf5_file, train_file)
+                output_file.append((hdf5_file, train_file))
+    with open(options.hdf5_output_list, "w"):
+        for line in output_file:
+            f.write(f'{line[0]} {line[1]}\n')
     
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input_file', dest='input_file',
-                        type=str, required=False, help='input hdf5 file')
-    parser.add_argument('-o', '--output_file', dest='output_file',
-                        type=str, required=True, help='output hdf5 file')
+    parser.add_argument('-hl', '--hdf5_list', dest='hdf5_list',
+                        type=str, required=False, help='list with hdf5 pathes')
+    parser.add_argument('-hol', '--hdf5_output_list', dest='hdf5_output_list',
+                        type=str, required=True, help='list with new hdf5 pathes with corresponding train_X.hdf5 path')
+    parser.add_argument('-pref', '--prefix_path', dest='prefix_path',
+                        type=str, required=True, help='path prefix to store symlinks')
     return parser.parse_args()
 
 if __name__ == '__main__':
