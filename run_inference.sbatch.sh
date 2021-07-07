0d8d3e1a9cf66641b4e5209ab093d30c7782f455
Alexey Artemov <a.artemov@skoltech.ru>
2021-07-07 17:41:15 +0300

    fix -f transform_type arg

--- run_inference.sbatch.sh
+++ run_inference.sbatch.sh
@@ -38,7 +38,7 @@ TRANSFORM_TYPE=depth-regression-arbitrary
 SYSTEM=def-image-regression
 CHECKPOINT=
 
-while getopts "i:o:vt:d:c:m:s:l:" opt
+while getopts "i:o:vt:d:c:m:s:f:l:" opt
 do
     case ${opt} in
         i) INPUT_PATH_HOST=$OPTARG;;
@@ -48,6 +48,7 @@ do
         c) CHECKPOINT=$OPTARG;;
         m) MODEL_TYPE=$OPTARG;;
         s) SYSTEM=$OPTARG;;
+        f) TRANSFORM_TYPE=$OPTARG;;
         l) LOGS_PATH_HOST=$OPTARG;;
         v) VERBOSE=true;;
         *) usage; exit 1 ;;
