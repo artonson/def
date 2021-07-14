c83a835aa909d9cf0a5daca5e455ed72b4753d4c
emil <emilzq@bk.ru>
2021-07-14 10:51:16 +0000

    fix pathes

--- scripts/data_scripts/cmd_args_generators/args_images2points_fusion.py
+++ scripts/data_scripts/cmd_args_generators/args_images2points_fusion.py
@@ -13,20 +13,23 @@ def main():
     mapping_path = "/trinity/home/e.bogomolov/sharpf_data/images2points_patches_for_train.txt"
     output_file = "/trinity/home/e.bogomolov/sharpf_data/images2points_fusion.txt"
     symlinks_prefix = "/gpfs/gpfs0/3ddl/sharp_features/images2points/for_model"
+    gt_prefix = "/gpfs/gpfs0/3ddl/sharp_features/images2points/data_v2_cvpr/images_whole/"
 
     with open(mapping_path, "r") as f:
         for line in f:
             line = line.strip()
             gt_path, symlink_path = line.split(" ")
             symlink_path = str(pathlib.Path(symlink_path).relative_to(symlinks_prefix))
+            symlink_path = symlink_path.rstrip(".hdf5")
 
-            outdir = pathlib.Path(os.path.join(fusion_prefix, symlink_path))
+            outdir = str(pathlib.Path(gt_path).relative_to(gt_prefix)).rstrip("patches.hdf5")
+            outdir = pathlib.Path(os.path.join(fusion_prefix, outdir))
             try:
                 outdir.mkdir(parents=True, exist_ok=False)
             except FileExistsError:
                 pass
             
-            predict_path = os.path.join(predicts_prefix, symlink_path)
+            predict_path = os.path.join(predicts_prefix, symlink_path, "predictions")
             if os.path.exists(predict_path):
                 arguments.append((
                     gt_path, 
