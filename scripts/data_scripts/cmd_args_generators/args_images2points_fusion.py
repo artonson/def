f94b8f57c2d652e06214afdee87159a1f6812fc5
emil <emilzq@bk.ru>
2021-07-14 10:28:03 +0000

    fix and prettify args generator

--- scripts/data_scripts/cmd_args_generators/args_images2points_fusion.py
+++ scripts/data_scripts/cmd_args_generators/args_images2points_fusion.py
@@ -8,18 +8,17 @@ import argparse
 def main():
     arguments = []
 
-    predicts_prefix = "/gpfs/data/gpfs0/3ddl/sharp_features/predictions/images2points/for_model/"
+    predicts_prefix = "/gpfs/gpfs0/3ddl/sharp_features/images2points/predictions/"
     fusion_prefix = "/gpfs/gpfs0/3ddl/sharp_features/images2points/fused"
     mapping_path = "/trinity/home/e.bogomolov/sharpf_data/images2points_patches_for_train.txt"
     output_file = "/trinity/home/e.bogomolov/sharpf_data/images2points_fusion.txt"
+    symlinks_prefix = "/gpfs/gpfs0/3ddl/sharp_features/images2points/for_model"
 
     with open(mapping_path, "r") as f:
         for line in f:
             line = line.strip()
             gt_path, symlink_path = line.split(" ")
-            symlink_path = str(pathlib.Path(symlink_path).relative_to("/gpfs/gpfs0/3ddl/sharp_features/images2points/for_model"))
-            symlink_path = symlink_path.replace("_res_whole.json", "")
-            symlink_path = symlink_path.replace(".hdf5", "")
+            symlink_path = str(pathlib.Path(symlink_path).relative_to(symlinks_prefix))
 
             outdir = pathlib.Path(os.path.join(fusion_prefix, symlink_path))
             try:
@@ -36,7 +35,6 @@ def main():
     with open(output_file, "w") as f:
         for args in arguments:
             f.write(f'{args[0]} {args[1]} {args[2]} 0.9\n')
-
 
 
 if __name__ == '__main__':
