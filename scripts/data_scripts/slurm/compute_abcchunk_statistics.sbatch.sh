13f701d41b97d1417739db47666b34705fc340e7
Alexey Artemov <a.artemov@skoltech.ru>
2022-04-16 14:27:23 +0300

    update compute_abcchunk_statistics.sbatch.sh to run in parallel mode

--- scripts/data_scripts/slurm/compute_abcchunk_statistics.sbatch.sh
+++ scripts/data_scripts/slurm/compute_abcchunk_statistics.sbatch.sh
@@ -10,10 +10,10 @@
 #SBATCH --mem-per-cpu=20g
 
 __usage="
-Usage: $0 -a abc_input_dir -o output_file [-v]
+Usage: $0 -a abc_input_dir -o output_dir [-c chunk] [-n N1] [-m N2] [-v]
 
   -a: 	input ABC directory
-  -c: 	input ABC chunk number
+  -c: 	if set, input ABC chunk number; if unset, --array must be specified
   -o: 	output file with resulting output
   -v:   if set, verbose mode is activated (more output from the script generally)
 "
@@ -27,7 +27,7 @@ do
     case ${opt} in
         c) CHUNK=$OPTARG;;
         a) ABC_PATH_HOST=$OPTARG;;
-        o) OUTPUT_FILE_HOST=$OPTARG;;
+        o) OUTPUT_PATH_HOST=$OPTARG;;
         v) VERBOSE=true;;
         n) ID_START=$OPTARG;;
         m) ID_END=$OPTARG;;
@@ -42,26 +42,38 @@ fi
 
 if [[ -n "${ID_START}" ]]; then
     ID_START_ARG="-n1=${ID_START}"
+else
+    ID_START="all"
 fi
 if [[ -n "${ID_END}" ]]; then
     ID_END_ARG="-n2=${ID_END}"
+else
+    ID_END="all"
 fi
-
-# get image filenames from here
-PROJECT_ROOT=/trinity/home/a.artemov/repos/sharp_features2
-source "${PROJECT_ROOT}"/env.sh
 
 ABC_PATH_CONTAINER="/abc"
 if [[ ! ${ABC_PATH_HOST} ]]; then
     echo "input_dir is not set" && usage && exit 1
 fi
 
+if [[ -n "${CHUNK}" ]]; then
+    if [[ -n "${SLURM_ARRAY_TASK_ID}" ]]; then
+        CHUNK=${SLURM_ARRAY_TASK_ID}
+    else
+      echo "chunk is not set and --array is not specified" && usage && exit 1
+    fi
+fi
+
 OUTPUT_PATH_CONTAINER="/out"
 if [[ ! ${OUTPUT_FILE_HOST} ]]; then
     echo "output_dir is not set" && usage && exit 1
 fi
-OUTPUT_PATH_HOST=$( dirname "${OUTPUT_FILE_HOST}" )
-OUTPUT_BASENAME=$( basename "${OUTPUT_FILE_HOST}" )
+OUTPUT_BASENAME="abc_statistics__${CHUNK}__${ID_START}__${ID_END}.txt"
+OUTPUT_FILE="${OUTPUT_PATH_CONTAINER}/${OUTPUT_BASENAME}"
+
+# get image filenames from here
+PROJECT_ROOT=/trinity/home/a.artemov/repos/sharp_features2
+source "${PROJECT_ROOT}"/env.sh
 
 CODE_PATH_CONTAINER="/code"
 CODE_PATH_HOST=${PROJECT_ROOT}
@@ -81,7 +93,6 @@ echo "  "
 
 N_TASKS=$(( SLURM_CPUS_PER_TASK ))
 SCRIPT="${CODE_PATH_CONTAINER}/scripts/data_scripts/compute_abcchunk_statistics.py"
-OUTPUT_FILE="${OUTPUT_PATH_CONTAINER}/${OUTPUT_BASENAME}"
 
 singularity exec \
   --bind ${CODE_PATH_HOST}:${CODE_PATH_CONTAINER} \
